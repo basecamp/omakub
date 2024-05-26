df05ad65926b23eb916695e5f3e585285e97f839
David Heinemeier Hansson <david@hey.com>
2024-05-25 19:49:54 -0700

    Move starters into their own directory

--- install.sh
+++ install.sh
@@ -23,5 +23,4 @@ source install/*.sh
 gh auth login
 
 # Start
-docker run -d --restart unless-stopped -p 3306:3306 --name=mysql8 -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:8
-docker run -d --restart unless-stopped -p 6379:6379 --name=redis redis
+source start/containers.sh
---
+++ start/containers.sh
@@ -0,0 +1,2 @@
+docker run -d --restart unless-stopped -p 3306:3306 --name=mysql8 -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:8
+docker run -d --restart unless-stopped -p 6379:6379 --name=redis redis
