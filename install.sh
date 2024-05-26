a1f9bbddbce331b51a08a45afd1b76b229f2d0d0
David Heinemeier Hansson <david@hey.com>
2024-05-26 15:38:30 -0700

    Announce each section

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # Libraries and infrastructure
+echo -e "\e[32mINSTALLING CORE DEVELOPMENT LIBRARIES\e[0m"
 sudo apt update -y
 sudo apt install -y \
 	docker.io docker-buildx \
@@ -8,15 +9,18 @@ sudo apt install -y \
 	redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev
 
 # CLI apps
+echo -e "\e[32mINSTALLING CLI APPSe[0m"
 sudo apt install -y git curl fzf ripgrep bat eza zoxide btop apache2-utils
 sudo snap install zellij --classic
 
 # GUI apps
+echo -e "\e[32mINSTALLING GUI APPSe[0m"
 sudo apt install -y xournalpp alacritty
 sudo snap install 1password spotify vlc zoom-client signal-desktop pinta
 sudo snap install code --classic
 
 # Installers
+echo -e "\e[32mRUNNING CUSTOM INSTALLERSe[0m"
 for script in ~/.omakub/install/*.sh; do source $script; done
 
 # Start services
