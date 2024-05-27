1eedf41057e6ad86f238b4dbf44c92aad05fae0c
David Heinemeier Hansson <david@hey.com>
2024-05-26 18:43:15 -0700

    Extract node version

--- install/nodenv.sh
+++ install/nodenv.sh
@@ -1,4 +1,6 @@
 if ! command -v nodenv &>/dev/null; then
+	DEFAULT_NODE_VERSION="20.13.1"
+
 	git clone https://github.com/nodenv/nodenv.git ~/.nodenv
 	sudo ln -vs ~/.nodenv/bin/nodenv /usr/local/bin/nodenv
 	cd ~/.nodenv
@@ -7,7 +9,7 @@ if ! command -v nodenv &>/dev/null; then
 	mkdir -p "$(nodenv root)"/plugins
 	git clone https://github.com/nodenv/node-build.git "$(nodenv root)"/plugins/node-build
 	git clone https://github.com/nodenv/nodenv-aliases.git $(nodenv root)/plugins/nodenv-aliases
-	nodenv install 20.11.1
-	nodenv global 20.11.1
+	nodenv install $DEFAULT_NODE_VERSION
+	nodenv global $DEFAULT_NODE_VERSION
 	sudo ln -vs $(nodenv root)/shims/* /usr/local/bin/
 fi
