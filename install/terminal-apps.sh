0c4353c19be1b277601615ab6f640199c6197693
David Heinemeier Hansson <david@hey.com>
2024-05-27 13:04:48 -0700

    Setup zellij directly and use external theme

--- install/terminal-apps.sh
+++ install/terminal-apps.sh
@@ -1,2 +1,1 @@
 sudo apt install -y fzf ripgrep bat eza zoxide btop apache2-utils
-sudo snap install zellij --classic
---
+++ install/zellij.sh
@@ -0,0 +1,6 @@
+if ! command -v zellij &>/dev/null; then
+	sudo snap install zellij --classic
+	mkdir -p ~/.config/zellij/
+	cp ~/.local/share/omakub/dotfiles/zellij.kdl ~/.config/zellij/config.kdl
+	ln -s ~/.local/share/omakub/themes/zellij ~/.config/zellij/themes
+fi
