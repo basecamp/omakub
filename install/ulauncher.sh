730a1c86ae2aa9fb1fa39a605b1a13c1022b5cb1
David Heinemeier Hansson <david@hey.com>
2024-05-26 21:31:43 -0700

    Remember to setup the keybinding

--- install/ulauncher.sh
+++ install/ulauncher.sh
@@ -3,4 +3,9 @@ if ! command -v ulauncher &>/dev/null; then
 	sudo add-apt-repository ppa:agornostal/ulauncher -y
 	sudo apt update -y
 	sudo apt install -y ulauncher
+
+	# FIXME: Install this keybinding
+	# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom0 binding '<Super>space'
+	# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom0 command 'ulauncher-toggle'
+	# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom0 name 'ulauncher'
 fi
