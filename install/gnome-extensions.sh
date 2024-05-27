4db98a6b915eb945b16993bd0e83676eeddb2b3a
David Heinemeier Hansson <david@hey.com>
2024-05-26 21:26:12 -0700

    Protect against rerunning

--- install/gnome-extensions.sh
+++ install/gnome-extensions.sh
@@ -1,43 +1,46 @@
-sudo apt install -y gnome-shell-extension-manager pipx
-pipx install gnome-extensions-cli --system-site-packages
+if ! command -v gnome-shell-extension-manager &>/dev/null; then
+	sudo apt install -y gnome-shell-extension-manager pipx
+	pipx install gnome-extensions-cli --system-site-packages
 
-gext install tactile@lundal.io
-gext install just-perfection-desktop@just-perfection
-gext install blur-my-shell@aunetx
+	# FIXME: Break these extensions into individual installers
+	gext install tactile@lundal.io
+	gext install just-perfection-desktop@just-perfection
+	gext install blur-my-shell@aunetx
 
-# Compile gsettings schemas in order to be able to set them
-sudo cp ~/.local/share/gnome-shell/extensions/tactile@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml /usr/share/glib-2.0/schemas/
-sudo cp ~/.local/share/gnome-shell/extensions/just-perfection-desktop\@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml /usr/share/glib-2.0/schemas/
-sudo cp ~/.local/share/gnome-shell/extensions/blur-my-shell\@aunetx/schemas/org.gnome.shell.extensions.blur-my-shell.gschema.xml /usr/share/glib-2.0/schemas/
-sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
+	# Compile gsettings schemas in order to be able to set them
+	sudo cp ~/.local/share/gnome-shell/extensions/tactile@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml /usr/share/glib-2.0/schemas/
+	sudo cp ~/.local/share/gnome-shell/extensions/just-perfection-desktop\@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml /usr/share/glib-2.0/schemas/
+	sudo cp ~/.local/share/gnome-shell/extensions/blur-my-shell\@aunetx/schemas/org.gnome.shell.extensions.blur-my-shell.gschema.xml /usr/share/glib-2.0/schemas/
+	sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
 
-# Configure Tactile
-gsettings set org.gnome.shell.extensions.tactile col-0 1
-gsettings set org.gnome.shell.extensions.tactile col-0 1
-gsettings set org.gnome.shell.extensions.tactile col-1 2
-gsettings set org.gnome.shell.extensions.tactile col-2 1
-gsettings set org.gnome.shell.extensions.tactile col-3 0
-gsettings set org.gnome.shell.extensions.tactile row-0 1
-gsettings set org.gnome.shell.extensions.tactile row-1 1
+	# Configure Tactile
+	gsettings set org.gnome.shell.extensions.tactile col-0 1
+	gsettings set org.gnome.shell.extensions.tactile col-0 1
+	gsettings set org.gnome.shell.extensions.tactile col-1 2
+	gsettings set org.gnome.shell.extensions.tactile col-2 1
+	gsettings set org.gnome.shell.extensions.tactile col-3 0
+	gsettings set org.gnome.shell.extensions.tactile row-0 1
+	gsettings set org.gnome.shell.extensions.tactile row-1 1
 
-# Configure Just Perfection
-gsettings set org.gnome.shell.extensions.just-perfection animation 2
-gsettings set org.gnome.shell.extensions.just-perfection dash-app-running true
-gsettings set org.gnome.shell.extensions.just-perfection workspace true
-gsettings set org.gnome.shell.extensions.just-perfection workspace-popup false
+	# Configure Just Perfection
+	gsettings set org.gnome.shell.extensions.just-perfection animation 2
+	gsettings set org.gnome.shell.extensions.just-perfection dash-app-running true
+	gsettings set org.gnome.shell.extensions.just-perfection workspace true
+	gsettings set org.gnome.shell.extensions.just-perfection workspace-popup false
 
-# Configure Blur My Shell
-gsettings set org.gnome.shell.extensions.blur-my-shell.appfolder blur false
-gsettings set org.gnome.shell.extensions.blur-my-shell.lockscreen blur false
-gsettings set org.gnome.shell.extensions.blur-my-shell.screenshot blur false
-gsettings set org.gnome.shell.extensions.blur-my-shell.window-list blur false
-gsettings set org.gnome.shell.extensions.blur-my-shell.panel brightness 0.6
-gsettings set org.gnome.shell.extensions.blur-my-shell.panel sigma 30
-gsettings set org.gnome.shell.extensions.blur-my-shell.panel pipeline 'pipeline_default'
-gsettings set org.gnome.shell.extensions.blur-my-shell.overview blur true
-gsettings set org.gnome.shell.extensions.blur-my-shell.overview pipeline 'pipeline_default'
-gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock blur true
-gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock brightness 0.6
-gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock sigma 30
-gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock static-blur true
-gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock style-dash-to-dock 0
+	# Configure Blur My Shell
+	gsettings set org.gnome.shell.extensions.blur-my-shell.appfolder blur false
+	gsettings set org.gnome.shell.extensions.blur-my-shell.lockscreen blur false
+	gsettings set org.gnome.shell.extensions.blur-my-shell.screenshot blur false
+	gsettings set org.gnome.shell.extensions.blur-my-shell.window-list blur false
+	gsettings set org.gnome.shell.extensions.blur-my-shell.panel brightness 0.6
+	gsettings set org.gnome.shell.extensions.blur-my-shell.panel sigma 30
+	gsettings set org.gnome.shell.extensions.blur-my-shell.panel pipeline 'pipeline_default'
+	gsettings set org.gnome.shell.extensions.blur-my-shell.overview blur true
+	gsettings set org.gnome.shell.extensions.blur-my-shell.overview pipeline 'pipeline_default'
+	gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock blur true
+	gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock brightness 0.6
+	gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock sigma 30
+	gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock static-blur true
+	gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock style-dash-to-dock 0
+fi
--- install/gnome-tailoring.sh
+++ install/gnome-tailoring.sh
@@ -1,9 +1,11 @@
-sudo apt install -y gnome-tweak-tool
+if ! command -v gnome-tweak-tool &>/dev/null; then
+	sudo apt install -y gnome-tweak-tool
 
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.gnome.desktop.interface cursor-theme 'Yaru'
-gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-purple-dark'
-gsettings set org.gnome.desktop.interface icon-theme 'Yaru-purple'
+	gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+	gsettings set org.gnome.desktop.interface cursor-theme 'Yaru'
+	gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-purple-dark'
+	gsettings set org.gnome.desktop.interface icon-theme 'Yaru-purple'
 
-# FIXME: Only do this on Framework laptops or similar displays
-gsettings set org.gnome.desktop.interface text-scaling-factor 0.8
+	# FIXME: Only do this on Framework laptops or similar displays
+	gsettings set org.gnome.desktop.interface text-scaling-factor 0.8
+fi
