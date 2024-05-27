423f0393ba7aa953a6ff40ae165bd41ebeef4d52
David Heinemeier Hansson <david@hey.com>
2024-05-26 19:47:00 -0700

    Cleanup

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -2,32 +2,32 @@
 mkdir -p ~/.config
 
 # Link all dotfiles
-for entry in dotfiles/*; do
+for entry in ~/.local/share/omakub/dotfiles/*; do
 	# Link all root files as .file in ~/
 	# Any existing files will be renamed .bak
-	if [ -f "$entry" ]; then
-		target=~/."$(basename "$entry")"
+	if [ -f $entry ]; then
+		target=~/."$(basename $entry)"
 
-		if [ -e "$target" ] && [ "$(readlink "$target")" != "$(pwd)/$entry" ]; then
-			mv "$target" "$target.bak"
+		if [ -e $target ] && [ "$(readlink $target)" != $entry ]; then
+			mv $target $target.bak
 		fi
 
-		if [ ! -e "$target" ]; then
-			ln -s "$(pwd)/$entry" "$target"
+		if [ ! -e $target ]; then
+			ln -s $entry $target
 		fi
 	fi
 
 	# Link all directories in ~/.config/
 	# Any existing directories will be renamed .bak
-	if [ -d "$entry" ]; then
-		target=~/.config/"$(basename "$entry")"
+	if [ -d $entry ]; then
+		target=~/.config/"$(basename $entry)"
 
-		if [ -e "$target" ] && [ "$(readlink "$target")" != "$(pwd)/$entry" ]; then
-			mv "$target" "$target.bak"
+		if [ -e $target ] && [ "$(readlink "$target")" != $entry ]; then
+			mv $target $target.bak
 		fi
 
-		if [ ! -e "$target" ]; then
-			ln -s "$(pwd)/$entry" "$target"
+		if [ ! -e $target ]; then
+			ln -s $entry $target
 		fi
 	fi
 done
