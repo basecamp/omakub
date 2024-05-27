273ee3f4d5be2e1df74265968b5b5e579822a34f
David Heinemeier Hansson <david@hey.com>
2024-05-26 18:37:47 -0700

    Arrangement

--- install/docker.sh
+++ install/docker.sh
@@ -1,8 +1,8 @@
 if ! command -v docker &>/dev/null; then
-	DOCKER_COMPOSE_VERSION="2.27.0"
 	sudo apt install -y docker.io docker-buildx
-
 	sudo usermod -aG docker ${USER}
+
+	DOCKER_COMPOSE_VERSION="2.27.0"
 	DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 	mkdir -p $DOCKER_CONFIG/cli-plugins
 	curl -SL https://github.com/docker/compose/releases/download/v$DOCKER_COMPOSE_VERSION/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
