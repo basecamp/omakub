c63b0930db02e5dcd82e31f8bd7a288a48745840
David Heinemeier Hansson <david@hey.com>
2024-05-27 20:12:46 -0700

    Split out the framework text scaling fix

---
+++ install/fix-framework-text-scaling.sh
@@ -0,0 +1,2 @@
+# FIXME: Only do this on Framework laptops or similar displays
+gsettings set org.gnome.desktop.interface text-scaling-factor 0.8
--- install/gnome-tailoring.sh
+++ install/gnome-tailoring.sh
@@ -1,6 +1,3 @@
 if ! command -v gnome-tweak-tool &>/dev/null; then
 	sudo apt install -y gnome-tweak-tool
 fi
-
-# FIXME: Only do this on Framework laptops or similar displays
-gsettings set org.gnome.desktop.interface text-scaling-factor 0.8
