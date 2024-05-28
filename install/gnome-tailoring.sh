d40779d50fe6bc5726227e31e5569bfe37ceda17
David Heinemeier Hansson <david@hey.com>
2024-05-27 20:12:05 -0700

    Split out default theme installation

--- install/gnome-tailoring.sh
+++ install/gnome-tailoring.sh
@@ -5,8 +5,5 @@ fi
 # Turn off default Ubuntu extensions
 gsettings set org.gnome.shell disabled-extensions ['tiling-assistant@ubuntu.com', 'ubuntu-appindicators@ubuntu.com', 'ubuntu-dock@ubuntu.com']
 
-# Set default UI theme
-source ~/.local/share/omakub/themes/gnome/yaru-dark-purple.sh
-
 # FIXME: Only do this on Framework laptops or similar displays
 gsettings set org.gnome.desktop.interface text-scaling-factor 0.8
---
+++ install/theme.sh
@@ -0,0 +1,2 @@
+# Setup default theme
+source ~/.local/share/omakub/themes/gnome/yaru.sh
