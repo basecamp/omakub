47a8954ffded672799ca7d6b508199814ce962e3
David Heinemeier Hansson <david@hey.com>
2024-05-27 19:15:42 -0700

    Turn off default extensions

--- install/gnome-tailoring.sh
+++ install/gnome-tailoring.sh
@@ -2,6 +2,8 @@ if ! command -v gnome-tweak-tool &>/dev/null; then
 	sudo apt install -y gnome-tweak-tool
 fi
 
+# Turn off default Ubuntu extensions
+gsettings set org.gnome.shell disabled-extensions ['tiling-assistant@ubuntu.com', 'ubuntu-appindicators@ubuntu.com', 'ubuntu-dock@ubuntu.com']
 
 # Set default UI theme
 source ~/.local/share/omakub/themes/gnome/yaru-dark-purple.sh
