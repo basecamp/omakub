038c96b05c3df829d946d869813ad553b1ead5dd
David Heinemeier Hansson <david@hey.com>
2024-05-27 19:15:49 -0700

    Moved

--- install/gnome-tailoring.sh
+++ install/gnome-tailoring.sh
@@ -8,7 +8,5 @@ gsettings set org.gnome.shell disabled-extensions ['tiling-assistant@ubuntu.com'
 # Set default UI theme
 source ~/.local/share/omakub/themes/gnome/yaru-dark-purple.sh
 
-	gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
-
 	# FIXME: Only do this on Framework laptops or similar displays
 	gsettings set org.gnome.desktop.interface text-scaling-factor 0.8
