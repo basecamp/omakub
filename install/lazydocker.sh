522b144db6961eec23721fdf5a3b22ea6e915b78
David Heinemeier Hansson <david@hey.com>
2024-05-29 14:18:51 -0700

    Use same installer for upgrading these
    Since there's no auto-updating otherwise

--- install/lazydocker.sh
+++ install/lazydocker.sh
@@ -1,9 +1,7 @@
-if ! command -v lazydocker &>/dev/null; then
-	cd ~/Downloads
-	LAZYDOCKER_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazydocker/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-	curl -sLo lazydocker.tar.gz "https://github.com/jesseduffield/lazydocker/releases/latest/download/lazydocker_${LAZYDOCKER_VERSION}_Linux_x86_64.tar.gz"
-	tar -xf lazydocker.tar.gz lazydocker
-	sudo install lazydocker /usr/local/bin
-	rm lazydocker.tar.gz
-	cd -
-fi
+cd ~/Downloads
+LAZYDOCKER_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazydocker/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -sLo lazydocker.tar.gz "https://github.com/jesseduffield/lazydocker/releases/latest/download/lazydocker_${LAZYDOCKER_VERSION}_Linux_x86_64.tar.gz"
+tar -xf lazydocker.tar.gz lazydocker
+sudo install lazydocker /usr/local/bin
+rm lazydocker.tar.gz
+cd -
