be0895197ec81c2c1ad9490018669e8d77a7739e
David Heinemeier Hansson <david@hey.com>
2024-05-29 14:21:57 -0700

    New display coming!

--- install/fix-framework-text-scaling.sh
+++ install/fix-framework-text-scaling.sh
@@ -1,2 +1,2 @@
-# FIXME: Only do this on Framework laptops or similar displays
+# FIXME: Only do this on Framework laptops with the lower-resolution display (or similar displays)
 gsettings set org.gnome.desktop.interface text-scaling-factor 0.8
