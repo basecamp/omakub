c6ad457ad08a931cc91d8b7ee59226bcc60dcdd7
David Heinemeier Hansson <david@hey.com>
2024-05-28 20:36:24 -0700

    Fix gsettings signature

--- install/gnome-hotkeys.sh
+++ install/gnome-hotkeys.sh
@@ -1,8 +1,8 @@
 # Alt+F4 is very cumbersome
-gsettings set org.gnome.desktop.wm.keybindings close ['<Super>w']
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>w']"
 
 # Full-screen with title/navigation bar
-gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen ['<Shift>F11']
+gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Shift>F11']"
 
 # Expand from 4 to 6 default workspaces
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
@@ -19,9 +19,9 @@ gsettings set org.gnome.shell.keybindings switch-to-application-8 "['<Alt>8']"
 gsettings set org.gnome.shell.keybindings switch-to-application-9 "['<Alt>9']"
 
 # Use super for workspaces
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 ['<Super>1']
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 ['<Super>2']
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 ['<Super>3']
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 ['<Super>4']
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 ['<Super>5']
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 ['<Super>6']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
