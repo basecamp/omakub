b0b69b314fb7cda9dc23564d9279d748f4f519e8
David Heinemeier Hansson <david@hey.com>
2024-05-30 16:57:51 -0700

    Use gnome-extensions cli to disable instead of gsettings

--- install/gnome-extensions.sh
+++ install/gnome-extensions.sh
@@ -3,7 +3,10 @@ if ! command -v gnome-shell-extension-manager &>/dev/null; then
 	pipx install gnome-extensions-cli --system-site-packages
 
 	# Turn off default Ubuntu extensions
-	gsettings set org.gnome.shell disabled-extensions ['tiling-assistant@ubuntu.com', 'ubuntu-appindicators@ubuntu.com', 'ubuntu-dock@ubuntu.com']
+	gnome-extensions disable tiling-assistant@ubuntu.com
+	gnome-extensions disable ubuntu-appindicators@ubuntu.com
+	gnome-extensions disable ubuntu-dock@ubuntu.com
+	gnome-extensions disable ding@rastersoft.com
 
 	# FIXME: Break these extensions into individual installers
 	gext install tactile@lundal.io
