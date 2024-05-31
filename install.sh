393ef46a8690b27621363d587b3aa6bfb945f642
David Heinemeier Hansson <david@hey.com>
2024-05-31 11:13:38 -0700

    Dont actually need to change the lock enabler

--- install.sh
+++ install.sh
@@ -6,14 +6,12 @@ sudo apt install -y curl git
 
 # Ensure computer doesn't go to sleep while installing
 gsettings set org.gnome.desktop.session idle-delay 0
-gsettings set org.gnome.desktop.screensaver lock-enabled false
 
 # Run installers
 for script in ~/.local/share/omakub/install/*.sh; do source $script; done
 
-# Revert to normal idle and lock settings
+# Revert to normal idle settings
 gsettings set org.gnome.desktop.session idle-delay 300
-gsettings set org.gnome.desktop.screensaver lock-enabled true
 
 # Reboot to pickup changes
 read -p "Ready to reboot the system for all settings to take effect? [Y/n]: " response
