c3854c6bd99b788ea859b42e93d7a08d0944465d
David Heinemeier Hansson <david@hey.com>
2024-05-30 17:23:47 -0700

    Install default ulauncher settings

--- install/ulauncher.sh
+++ install/ulauncher.sh
@@ -3,9 +3,5 @@ if ! command -v ulauncher &>/dev/null; then
 	sudo add-apt-repository ppa:agornostal/ulauncher -y
 	sudo apt update -y
 	sudo apt install -y ulauncher
-
-	# FIXME: Install this keybinding
-	# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom0 binding '<Super>space'
-	# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom0 command 'ulauncher-toggle'
-	# gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom0 name 'ulauncher'
+	cp ~/.local/share/omakub/configs/ulauncher.json ~/.config/ulauncher/settings.json
 fi
