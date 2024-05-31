b6fe75e67c7b07ee1be41abb4f11d8bda1bb0289
David Heinemeier Hansson <david@hey.com>
2024-05-31 10:52:21 -0700

    Zoom gives the better look

--- themes/gnome/yaru.sh
+++ themes/gnome/yaru.sh
@@ -6,4 +6,4 @@ gsettings set org.gnome.desktop.interface icon-theme 'Yaru-purple'
 OMAKUB_DEFAULT_BACKGROUND_IMAGE_PATH="$HOME/.local/share/omakub/backgrounds/80s-retro-tropical-sunset-by-freepik.jpg"
 gsettings set org.gnome.desktop.background picture-uri $OMAKUB_DEFAULT_BACKGROUND_IMAGE_PATH
 gsettings set org.gnome.desktop.background picture-uri-dark $OMAKUB_DEFAULT_BACKGROUND_IMAGE_PATH
-gsettings set org.gnome.desktop.background picture-options 'centered'
+gsettings set org.gnome.desktop.background picture-options 'zoom'
