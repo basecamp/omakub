25b0231eb0c609f202a10712b03957694b4bcb1f
David Heinemeier Hansson <david@hey.com>
2024-05-31 08:26:12 -0700

    Execute the new shell so path is setup

--- install/shell.sh
+++ install/shell.sh
@@ -1,5 +1,6 @@
 [ -f "~/.bashrc" ] && mv ~/.bashrc ~/.bashrc.bak
 cp ~/.local/share/omakub/configs/bashrc ~/.bashrc
+source ~/.bashrc
 
 [ -f "~/.inputrc" ] && mv ~/.inputrc ~/.inputrc.bak
 cp ~/.local/share/omakub/configs/inputrc ~/.inputrc
