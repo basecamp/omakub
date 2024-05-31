17e19847d54bfa20f3363760731be982a614b2f6
David Heinemeier Hansson <david@hey.com>
2024-05-30 20:21:41 -0700

    Prepare for easy swapping of alacritty font and theme

--- install/alacritty.sh
+++ install/alacritty.sh
@@ -2,4 +2,6 @@ if ! command -v alacritty &>/dev/null; then
 	sudo apt install -y alacritty
 	mkdir -p ~/.config/alacritty
 	cp ~/.local/share/omakub/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
+	cp ~/.local/share/omakub/configs/alacritty/theme.toml ~/.config/alacritty/theme.toml
+	cp ~/.local/share/omakub/configs/alacritty/font.toml ~/.config/alacritty/font.toml
 fi
