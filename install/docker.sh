09ce58bcf2161a6f59aea4ec3776dd762097d2d6
David Heinemeier Hansson <david@hey.com>
2024-05-31 08:25:02 -0700

    Reminder

--- install/docker.sh
+++ install/docker.sh
@@ -8,6 +8,7 @@ if ! command -v docker &>/dev/null; then
 	curl -sSL https://github.com/docker/compose/releases/download/v$DOCKER_COMPOSE_VERSION/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 	chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
+	# FIXME: Add postgresql as a default configured DB as well
 	sudo docker create --restart unless-stopped -p 3306:3306 --name=mysql8 -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:8
 	sudo docker create --restart unless-stopped -p 6379:6379 --name=redis redis
 fi
