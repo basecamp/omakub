2f8e31cfe820251d741484d060d347a2e5a633a8
David Heinemeier Hansson <david@hey.com>
2024-06-02 13:01:29 -0700

    Fine to key all hotkey settings in one file

--- install/flameshot.sh
+++ install/flameshot.sh
@@ -1,5 +1,4 @@
 # Flameshot is a nice step-up over the default Gnome screenshot tool
 if ! command -v flameshot &>/dev/null; then
 	sudo apt install -y flameshot
-	# FIXME: Move the hotkey setting over here when we have a command for it
 fi
