1d66cf1a953ab6ee3652ce86f47c0913313cb136
David Heinemeier Hansson <david@hey.com>
2024-06-02 15:06:16 -0700

    Install Tokyo Night theme for VSC

--- install/vscode.sh
+++ install/vscode.sh
@@ -2,4 +2,5 @@ if ! command -v code &>/dev/null; then
 	sudo snap install code --classic
 	mkdir -p ~/.config/Code/User
 	cp ~/.local/share/omakub/configs/vscode.json ~/.config/Code/User/settings.json
+	code --install-extension enkia.tokyo-night
 fi
