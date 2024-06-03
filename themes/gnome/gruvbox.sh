087a9e331c39ab6ae370e81155311e341d4dfdeb
David Heinemeier Hansson <david@hey.com>
2024-06-03 12:58:45 -0700

    Finish gruvbox theme

---
+++ themes/gnome/gruvbox.sh
@@ -0,0 +1,9 @@
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'Yaru'
+gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-sage-dark'
+gsettings set org.gnome.desktop.interface icon-theme 'Yaru-sage'
+
+OMAKUB_DEFAULT_BACKGROUND_IMAGE_PATH="$OMAKUB_PATH/backgrounds/ferns-green.jpg"
+gsettings set org.gnome.desktop.background picture-uri $OMAKUB_DEFAULT_BACKGROUND_IMAGE_PATH
+gsettings set org.gnome.desktop.background picture-uri-dark $OMAKUB_DEFAULT_BACKGROUND_IMAGE_PATH
+gsettings set org.gnome.desktop.background picture-options 'zoom'
