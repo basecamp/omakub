e3654165253fc4743b954b03316e37d88af147d0
David Heinemeier Hansson <david@hey.com>
2024-06-03 13:26:40 -0700

    Fix vscode theme changing too

--- install/vscode.sh
+++ install/vscode.sh
@@ -2,5 +2,9 @@ if ! command -v code &>/dev/null; then
 	sudo snap install code --classic
 	mkdir -p ~/.config/Code/User
 	cp $OMAKUB_PATH/configs/vscode.json ~/.config/Code/User/settings.json
+
+	# Install default supported themes
 	code --install-extension enkia.tokyo-night
+	code --install-extension jdinhlife.gruvbox
+	code --install-extension Catppuccin.catppuccin-vsc
 fi
