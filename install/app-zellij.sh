67864edec27f3c8423d2d6215b41f069fa96e341
David Heinemeier Hansson <david@hey.com>
2024-06-04 14:42:04 -0700

    Rescue Zellij from Snap

--- install/app-zellij.sh
+++ install/app-zellij.sh
@@ -1,6 +1,10 @@
-if [ -n "$FORCE" ] || ! command -v zellij &>/dev/null; then
-	sudo snap install zellij --classic
-	mkdir -p ~/.config/zellij/
-	cp ~/.local/share/omakub/configs/zellij.kdl ~/.config/zellij/config.kdl
-	ln -s ~/.local/share/omakub/themes/zellij ~/.config/zellij/themes
-fi
+cd ~/Downloads
+curl -sLo zellij.tar.gz "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz"
+tar -xf zellij.tar.gz zellij
+sudo install zellij /usr/local/bin
+rm zellij.tar.gz
+cd -
+
+mkdir -p ~/.config/zellij/
+cp ~/.local/share/omakub/configs/zellij.kdl ~/.config/zellij/config.kdl
+ln -s ~/.local/share/omakub/themes/zellij ~/.config/zellij/themes
