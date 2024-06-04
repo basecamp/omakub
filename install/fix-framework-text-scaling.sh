9b08be176e6a2ed77430825eac715047acfffd52
David Heinemeier Hansson <david@hey.com>
2024-06-03 17:16:25 -0700

    Fix match and condition

--- install/fix-framework-text-scaling.sh
+++ install/fix-framework-text-scaling.sh
@@ -1,6 +1,6 @@
-COMPUTER_MAKER=$(sudo dmidecode -t system | grep 'Manufacturer' | awk '{print $2}')
+COMPUTER_MAKER=$(sudo dmidecode -t system | grep 'Manufacturer:' | awk '{print $2}')
 SCREEN_RESOLUTION=$(xrandr | grep '*+' | awk '{print $1}')
 
-if [ "$COMPUTER_MAKER" == "Framework" && "$SCREEN_RESOLUTION" == "2256x1504" ]; then
+if [ "$COMPUTER_MAKER" == "Framework" ] && [ "$SCREEN_RESOLUTION" == "2256x1504" ]; then
 	gsettings set org.gnome.desktop.interface text-scaling-factor 0.8
 fi
