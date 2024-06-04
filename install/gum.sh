48a2dd3b321a7b4240356b285e3456853c60d9c3
David Heinemeier Hansson <david@hey.com>
2024-06-04 13:02:54 -0700

    Always use apt install

--- install/gum.sh
+++ install/gum.sh
@@ -2,6 +2,6 @@
 cd ~/Downloads
 GUM_VERSION="0.14.1" # Use known good version
 curl -sLo gum.deb "https://github.com/charmbracelet/gum/releases/latest/download/gum_${GUM_VERSION}_amd64.deb"
-sudo dpkg -i gum.deb
+sudo apt install ./gum.deb
 rm gum.deb
 cd -
