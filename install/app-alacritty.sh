f4f893d06a12d153b28d51fa613276790b58f80d
David Heinemeier Hansson <david@hey.com>
2024-06-04 14:51:09 -0700

    Drop protection idea

--- install/app-alacritty.sh
+++ install/app-alacritty.sh
@@ -1,7 +1,5 @@
-if [ -n "$FORCE" ] || ! command -v alacritty &>/dev/null; then
-	sudo apt install -y alacritty
-	mkdir -p ~/.config/alacritty
-	cp ~/.local/share/omakub/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
-	cp ~/.local/share/omakub/configs/alacritty/theme.toml ~/.config/alacritty/theme.toml
-	cp ~/.local/share/omakub/configs/alacritty/font.toml ~/.config/alacritty/font.toml
-fi
+sudo apt install -y alacritty
+mkdir -p ~/.config/alacritty
+cp ~/.local/share/omakub/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
+cp ~/.local/share/omakub/configs/alacritty/theme.toml ~/.config/alacritty/theme.toml
+cp ~/.local/share/omakub/configs/alacritty/font.toml ~/.config/alacritty/font.toml
