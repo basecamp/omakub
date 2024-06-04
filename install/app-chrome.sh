8715746a1965fd19c56f206554f4092dc489d7e0
David Heinemeier Hansson <david@hey.com>
2024-06-04 13:49:19 -0700

    No need for a guard

--- install/app-chrome.sh
+++ install/app-chrome.sh
@@ -1,7 +1,5 @@
-if [ -n "$FORCE" ] || ! command -v google-chrome &>/dev/null; then
-	cd ~/Downloads
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-	sudo dpkg -i google-chrome-stable_current_amd64.deb
-	rm google-chrome-stable_current_amd64.deb
-	cd -
-fi
+cd ~/Downloads
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+cd -
