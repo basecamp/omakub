7cc1b15030b3c913682e49281773ae728cd78762
David Heinemeier Hansson <david@hey.com>
2024-06-03 17:08:38 -0700

    Fix end clause

--- install/fix-framework-text-scaling.sh
+++ install/fix-framework-text-scaling.sh
@@ -1,3 +1,3 @@
 if [ $(xrandr | grep '*+' | awk '{print $1}') == "2256x1504" ]; then
-  gsettings set org.gnome.desktop.interface text-scaling-factor 0.8
-end
+	gsettings set org.gnome.desktop.interface text-scaling-factor 0.8
+fi
