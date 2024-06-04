117cdd9bee31aaa8d11f4091c22b4f206a8a9c79
David Heinemeier Hansson <david@hey.com>
2024-06-04 14:23:48 -0700

    Rescue Spotify from Snap

---
+++ install/app-spotify.sh
@@ -0,0 +1,4 @@
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/spotify.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
+sudo apt install -y spotify-client
--- install/gui-apps.sh
+++ install/gui-apps.sh
@@ -1,3 +1,2 @@
 sudo apt install -y xournalpp gnome-tweak-tool vlc
-sudo snap install spotify
 sudo snap install signal-desktop
