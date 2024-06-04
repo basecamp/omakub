4d37f8efd4dd8d3944e42570d038a0072062ef07
David Heinemeier Hansson <david@hey.com>
2024-06-04 13:03:11 -0700

    No need to guard, make it easier to upgrade

--- install/app-zoom.sh
+++ install/app-zoom.sh
@@ -1,7 +1,5 @@
-if [ -n "$FORCE" ] || ! command -v zoom &>/dev/null; then
-	cd ~/Downloads
-	wget https://zoom.us/client/latest/zoom_amd64.deb
-	sudo apt install ./zoom_amd64.deb
-	rm zoom_amd64.deb
-	cd -
-fi
+cd ~/Downloads
+wget https://zoom.us/client/latest/zoom_amd64.deb
+sudo apt install ./zoom_amd64.deb
+rm zoom_amd64.deb
+cd -
