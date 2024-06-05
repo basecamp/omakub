71b41d80b7e07cb5fbd89abb8eef968bb1684558
David Heinemeier Hansson <david@hey.com>
2024-06-05 09:42:01 -0700

    Set git aliases

---
+++ install/set-git.sh
@@ -0,0 +1,6 @@
+# Set common git aliases
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global pull.rebase true
