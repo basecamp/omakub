b37e2d6a3854d1206a3146c618fb0c539f6bf692
Iljo De Poorter <iljodp@gmail.com>
2024-06-06 19:19:32 +0200

    kanagawa?

---
+++ themes/gnome/kanagawa.sh
@@ -0,0 +1,23 @@
+
+# Download the theme
+wget https://github.com/Fausto-Korpsvart/Kanagawa-GKT-Theme/archive/refs/heads/main.zip -O kanagawa_theme.zip
+
+# Extract the theme
+unzip -q kanagawa_theme.zip -d ~/.themes/
+
+# Clean up
+rm kanagawa_theme.zip
+
+sudo apt update
+sudo apt install -y gtk2-engines-murrine gnome-themes-extra
+
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'Yaru'
+gsettings set org.gnome.desktop.interface gtk-theme 'Kanagawa'
+gsettings set org.gnome.desktop.interface icon-theme 'Kanagawa'
+
+OMAKUB_DEFAULT_BACKGROUND_IMAGE_PATH="$HOME/.local/share/omakub/backgrounds/kanagawa.jpg"
+gsettings set org.gnome.desktop.background picture-uri $OMAKUB_DEFAULT_BACKGROUND_IMAGE_PATH
+gsettings set org.gnome.desktop.background picture-uri-dark $OMAKUB_DEFAULT_BACKGROUND_IMAGE_PATH
+gsettings set org.gnome.desktop.background picture-options 'zoom'
+
