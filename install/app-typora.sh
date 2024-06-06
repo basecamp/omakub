3619ad5c975a7799677c4120532dff84df988cba
Alessandro Tagliapietra <tagliapietra.alessandro@gmail.com>
2024-06-05 22:16:45 -0700

    Use typora.io gpg key
    typoraio.cn hangs for me, the deb repo is using typora.io anyway and the typora.io website uses that domain as the one hosting the gpg key

--- install/app-typora.sh
+++ install/app-typora.sh
@@ -1,4 +1,4 @@
-wget -qO - https://typoraio.cn/linux/public-key.asc | sudo tee /etc/apt/trusted.gpg.d/typora.asc
+wget -qO - https://typora.io/linux/public-key.asc | sudo tee /etc/apt/trusted.gpg.d/typora.asc
 sudo add-apt-repository 'deb https://typora.io/linux ./'
 sudo apt update
 sudo apt install -y typora
