0843edda9538e01285d2a8a414c6e860093c6221
Salajan Silviu <salanoid@gmail.com>
2024-06-06 22:28:36 +0300

    Update app-neovim.sh - install from prebuilt bin
    Install Neovim from a prebuilt file following the exact same procedure we use for Zellij.

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -1,10 +1,9 @@
-cd ~/Downloads/
-git clone https://github.com/neovim/neovim
-cd neovim
-git checkout stable
-make CMAKE_BUILD_TYPE=Release
-make install
-cd ..; rm -rf neovim; cd ~/
+cd ~/Downloads
+wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
+tar -xf nvim.tar.gz
+sudo install nvim-linux64/bin/nvim /usr/local/bin/nvim
+rm -rf nvim-linux64 nvim.tar.gz
+cd -
 
 if [ ! -d "$HOME/.config/nvim" ]; then
 	git clone https://github.com/LazyVim/starter ~/.config/nvim
