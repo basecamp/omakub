c73f9c589b974265417c0a7e3b1ee6495aa86b82
David Heinemeier Hansson <david@hey.com>
2024-06-07 20:46:31 +0200

    Drop rbenv and nodenv in favor of mise
    Spelling

---
+++ install/mies.sh
@@ -0,0 +1,6 @@
+# Install mise for managing multiple versions of languages. See https://mise.jdx.dev/
+curl https://mise.run | sh
+
+# Install developer setups
+mise use --global ruby@latest
+mise use --global node@latest
