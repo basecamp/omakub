09b44d52c981acbb2d2f323bcda7570219113546
Totto16 <tobiausgais@gmail.com>
2024-06-07 12:54:43 +0200

    install.sh: reset 'idle-delay' to previous value, respecting the users settings, instead of defaulting to another specific value

--- install.sh
+++ install.sh
@@ -5,14 +5,17 @@ source ~/.local/share/omakub/ascii.sh
 sudo apt update -y
 sudo apt install -y curl git jq unzip
 
+# Get the current idle-delay, to restore it later
+IDLE_DELAY=$(gsettings get org.gnome.desktop.session idle-delay | cut -d " " -f 2)
+
 # Ensure computer doesn't go to sleep while installing
 gsettings set org.gnome.desktop.session idle-delay 0
 
 # Run installers
 for script in ~/.local/share/omakub/install/*.sh; do source $script; done
 
-# Revert to normal idle settings
-gsettings set org.gnome.desktop.session idle-delay 300
+# Revert to previous idle settings
+gsettings set org.gnome.desktop.session idle-delay "$IDLE_DELAY"
 
 # Upgrade everything that might ask for a reboot last
 sudo apt upgrade -y
