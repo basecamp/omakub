eea60a6ea64c1996a237cec4209707b54829d06b
David Heinemeier Hansson <david@hey.com>
2024-06-08 10:08:37 +0200

    Did not work for 3/4 of the extensions

--- install/set-gnome-extensions.sh
+++ install/set-gnome-extensions.sh
@@ -8,10 +8,10 @@ gnome-extensions disable ubuntu-dock@ubuntu.com
 gnome-extensions disable ding@rastersoft.com
 
 # Install new extensions
-gext --filesystem install tactile@lundal.io
-gext --filesystem install just-perfection-desktop@just-perfection
-gext --filesystem install blur-my-shell@aunetx
-gext --filesystem install space-bar@luchrioh
+gext install tactile@lundal.io
+gext install just-perfection-desktop@just-perfection
+gext install blur-my-shell@aunetx
+gext install space-bar@luchrioh
 
 # Compile gsettings schemas in order to be able to set them
 sudo cp ~/.local/share/gnome-shell/extensions/tactile@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml /usr/share/glib-2.0/schemas/
