f579e7d5609eddeafe631d12677d7b52b4ec7b0d
Martynas Puronas <martynas@puronas.me>
2024-06-08 08:24:05 +0300

    Add missing -y to scripts

--- install/app-localsend.sh
+++ install/app-localsend.sh
@@ -1,6 +1,6 @@
 cd ~/Downloads
 LOCALSEND_VERSION=$(curl -s "https://api.github.com/repos/localsend/localsend/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 wget -O localsend.deb "https://github.com/localsend/localsend/releases/latest/download/LocalSend-${LOCALSEND_VERSION}-linux-x86-64.deb"
-sudo apt install ./localsend.deb
+sudo apt install -y ./localsend.deb
 rm localsend.deb
 cd -
