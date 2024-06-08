cdcbcc3405af4c18d344a4c5a089e7c0e3322b06
David Heinemeier Hansson <david@hey.com>
2024-06-08 10:48:08 +0200

    Protect against screen locking during install as well

--- install.sh
+++ install.sh
@@ -5,17 +5,19 @@ source ~/.local/share/omakub/ascii.sh
 sudo apt update -y
 sudo apt install -y curl git unzip
 
-# Ensure computer doesn't go to sleep while installing
+# Ensure computer doesn't go to sleep or lock while installing
+gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.desktop.session idle-delay 0
 
 # Run installers
 for script in ~/.local/share/omakub/install/*.sh; do source $script; done
 
-# Revert to normal idle settings
-gsettings set org.gnome.desktop.session idle-delay 300
-
 # Upgrade everything that might ask for a reboot last
 sudo apt upgrade -y
+
+# Revert to normal idle and lock settings
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.session idle-delay 300
 
 # Logout to pickup changes
 gum confirm "Ready to logout for all settings to take effect?" && gnome-session-quit --logout --no-prompt
