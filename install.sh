eed06949f2f411b0bd056e913059b24720bb44e5
Maximilian Haack <max@haack.xyz>
2024-06-08 11:02:04 +0100

    Exit on error

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 # Be fancy
 source ~/.local/share/omakub/ascii.sh
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 # Needed for all installers
 sudo apt update -y
 sudo apt install -y curl git unzip
