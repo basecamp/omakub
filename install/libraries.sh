a0ce1969d5537b275f8568022a4345d062baec1c
Leonardo Betti <32095528+leonardobetti@users.noreply.github.com>
2024-06-10 17:06:55 +0100

    Update libraries.sh

--- install/libraries.sh
+++ install/libraries.sh
@@ -1,4 +1,4 @@
-sudo apt install -y \
+sudo nala install -y \
 	build-essential pkg-config autoconf bison rustc cargo clang \
 	libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
 	libvips imagemagick libmagickwand-dev mupdf mupdf-tools \
