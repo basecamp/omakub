2f8ff6d1eeb84bb70bc830600d6e100c50746bba
Leonardo Betti <32095528+leonardobetti@users.noreply.github.com>
2024-06-10 17:05:13 +0100

    Update app-typora.sh

--- install/app-typora.sh
+++ install/app-typora.sh
@@ -1,7 +1,7 @@
 wget -qO - https://typora.io/linux/public-key.asc | sudo tee /etc/apt/trusted.gpg.d/typora.asc
 sudo add-apt-repository -y 'deb https://typora.io/linux ./'
-sudo apt update
-sudo apt install -y typora
+sudo nala update
+sudo nala install -y typora
 
 # Add iA Typora theme
 mkdir -p ~/.config/Typora/themes
