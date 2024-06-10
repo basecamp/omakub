eb8555334a69fef5c736828bc7f425045e580321
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-06-10 23:31:54 +0530

    fix: typos in theme names

--- themes/neovim/tokyo-night.lua
+++ themes/neovim/tokyo-night.lua
@@ -1,18 +1,9 @@
 return {
+	{ "folke/tokyonight.nvim" },
 	{
 		"LazyVim/LazyVim",
 		opts = {
 			colorscheme = "tokyonight",
-		},
-	},
-	{
-		"folke/tokyonight.nvim",
-		opts = {
-			transparent = true,
-			styles = {
-				sidebars = "transparent",
-				floats = "transparent",
-			},
 		},
 	},
 }
