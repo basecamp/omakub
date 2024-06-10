a29fbc054ed9420a96d0effbca23633d51182f01
Leonardo Betti <32095528+leonardobetti@users.noreply.github.com>
2024-06-10 17:04:19 +0100

    Update app-neovim.sh

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -1,6 +1,6 @@
 sudo add-apt-repository -y ppa:neovim-ppa/stable
-sudo apt update
-sudo apt install -y neovim
+sudo nala update
+sudo nala install -y neovim
 
 if [ ! -d "$HOME/.config/nvim" ]; then
 	git clone https://github.com/LazyVim/starter ~/.config/nvim
