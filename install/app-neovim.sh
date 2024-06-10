e8548954acba4bd58bbaadb7cfbf5c34e65e3e64
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-06-10 18:11:36 +0530

    feat: copy commons file during installation

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -4,5 +4,6 @@ sudo apt install -y neovim
 
 if [ ! -d "$HOME/.config/nvim" ]; then
 	git clone https://github.com/LazyVim/starter ~/.config/nvim
+	cp ~/.local/share/omakub/themes/neovim/commons.lua ~/.config/nvim/lua/plugins/
 	cp ~/.local/share/omakub/themes/neovim/tokyo-night.lua ~/.config/nvim/lua/plugins/theme.lua
 fi
