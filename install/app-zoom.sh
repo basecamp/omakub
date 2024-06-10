0e96d173f6c2fb0adb39f3b4804ba3a51bcc6a26
Iljo De Poorter <iljodp@gmail.com>
2024-06-10 16:37:43 +0200

    added a zoom webapp when using arm64, this uses Chromium..

--- install/app-zoom.sh
+++ install/app-zoom.sh
@@ -4,6 +4,18 @@ if [[ $ARCH == "amd64" ]]; then
     sudo apt install -y ./zoom_amd64.deb
     rm zoom_amd64.deb
 elif [[ $ARCH == "arm64" ]]; then
-    echo "Zoom is only available for amd64 architecture."
+cat <<EOF > ~/.local/share/applications/Zoom.desktop
+[Desktop Entry]
+Version=1.0
+Name=Zoom
+Comment=Zoom online meetings
+Exec=chromium-browser --app="https://zoom.us/signin#/login" --name=Zoom
+Terminal=false
+Type=Application
+Icon=/home/$USER/.local/share/omakub/web-apps/icons/Zoom.png
+Categories=GTK;
+MimeType=text/html;text/xml;application/xhtml_xml;
+StartupNotify=true
+EOF
 fi
 cd -
