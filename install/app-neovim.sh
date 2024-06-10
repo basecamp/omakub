03c313b9600339f623e080c8cf52dd8c4b5c3c06
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-06-11 00:52:49 +0530

    fix: mkdir path for after folder

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -4,7 +4,7 @@ sudo apt install -y neovim
 
 if [ ! -d "$HOME/.config/nvim" ]; then
 	git clone https://github.com/LazyVim/starter ~/.config/nvim
-	mkdir -p ~/.config/nvim/lua/plugin/after
+	mkdir -p ~/.config/nvim/plugin/after
 	cp ~/.local/share/omakub/configs/neovim/transparency.lua ~/.config/nvim/plugin/after/
 	cp ~/.local/share/omakub/themes/neovim/tokyo-night.lua ~/.config/nvim/lua/plugins/theme.lua
 
