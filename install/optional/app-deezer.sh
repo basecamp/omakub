b556d2896e8dea985ff93eb47c471f81e577609d
Florent Destremau <florent@windoo.fr>
2024-06-11 16:03:03 +0200

    Added deezer install

---
+++ install/optional/app-deezer.sh
@@ -0,0 +1,2 @@
+wget https://github.com/aunetx/deezer-linux/releases/download/v6.0.110-1/deezer-desktop_6.0.110_amd64.deb -O ~/Downloads/deezer.deb
+sudo dpkg -i ~/Downloads/deezer.deb
---
+++ uninstall/app-deezer.sh
@@ -0,0 +1,1 @@
+sudo apt -y remove deezer-desktop
