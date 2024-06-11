1e36b3baf42e76ef5dfa96a3a0ffc5eb09d11723
David Heinemeier Hansson <david@hey.com>
2024-06-11 22:56:40 +0200

    Select dev env
    Here we do differ on what's needed/shared

--- install/mise.sh
+++ install/mise.sh
@@ -5,8 +5,3 @@ wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/
 echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
 sudo apt update
 sudo apt install -y mise
-
-# Install default languages
-mise use --global ruby@3.3
-mise use --global node@lts
-mise use --global go@latest
