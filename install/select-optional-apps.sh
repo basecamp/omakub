ac7f37e2f1d2889b88fc94a3c971f01961592aa0
David Heinemeier Hansson <david@hey.com>
2024-06-12 00:20:28 +0200

    Lowercase

--- install/select-optional-apps.sh
+++ install/select-optional-apps.sh
@@ -2,5 +2,5 @@
 apps=$(gum choose "1password" "Spotify" "Zoom" "Dropbox" "VirtualBox" --no-limit --selected "1password","Spotify","Zoom" --height 7 --header "Select commercial apps")
 
 for app in $apps; do
-	source "$OMAKUB_PATH/install/optional/app-$app.sh"
+	source "$OMAKUB_PATH/install/optional/app-${app,,}.sh"
 done
