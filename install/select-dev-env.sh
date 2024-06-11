78944be0d9da444836b1529c880c1a6540c3f353
David Heinemeier Hansson <david@hey.com>
2024-06-11 23:03:06 +0200

    Order defaults first

--- install/select-dev-env.sh
+++ install/select-dev-env.sh
@@ -22,18 +22,18 @@ for language in $languages; do
 done
 
 # Install default databases
-dbs=$(gum choose "MySQL" "PostgreSQL" "Redis" --no-limit --selected "MySQL","Redis" --height 5 --header "Select databases (runs in Docker)")
+dbs=$(gum choose "MySQL" "Redis" "PostgreSQL" --no-limit --selected "MySQL","Redis" --height 5 --header "Select databases (runs in Docker)")
 
 for db in $dbs; do
 	case $db in
 	MySQL)
 		sudo docker run -d --restart unless-stopped -p "127.0.0.1:3306:3306" --name=mysql8 -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:8.4
 		;;
-	PostgreSQL)
-		sudo docker run -d --restart unless-stopped -p "127.0.0.1:5432:5432" --name=postgres16 -e POSTGRES_HOST_AUTH_METHOD=trust postgres:16
-		;;
 	Redis)
 		sudo docker run -d --restart unless-stopped -p "127.0.0.1:6379:6379" --name=redis redis:7
+		;;
+	PostgreSQL)
+		sudo docker run -d --restart unless-stopped -p "127.0.0.1:5432:5432" --name=postgres16 -e POSTGRES_HOST_AUTH_METHOD=trust postgres:16
 		;;
 	esac
 done
