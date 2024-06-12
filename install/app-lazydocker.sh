9e18caa8746847e4d4679791f37dcc8553dc3e24
Iljodp <59059625+iljo-dp@users.noreply.github.com>
2024-06-12 12:01:53 +0200

    Apply suggestions from code review
    Co-authored-by: Jeremy Daer <jeremydaer@gmail.com>

--- install/app-lazydocker.sh
+++ install/app-lazydocker.sh
@@ -1,12 +1,13 @@
 cd ~/Downloads
 LAZYDOCKER_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazydocker/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 
-#For some reason, Lazydocker uses x86_64 instead of amd64 for naming, which means we can't use the variable ARCH for a 1 liner
-if [[ $ARCH == "arm64" ]]; then
-    curl -sLo lazydocker.tar.gz "https://github.com/jesseduffield/lazydocker/releases/latest/download/lazydocker_${LAZYDOCKER_VERSION}_Linux_${ARCH}.tar.gz"
-elif [[ $ARCH == "amd64" ]]; then
-    curl -sLo lazydocker.tar.gz "https://github.com/jesseduffield/lazydocker/releases/latest/download/lazydocker${LAZYDOCKER_VERSION}_Linux_x86_64.tar.gz"
+if [[ "$ARCH" == "arm64" ]]; then
+  LAZYDOCKER_ARCH="x86_64"
+else
+  LAZYDOCKER_ARCH="$ARCH"  
 fi
+
+curl -sLo lazydocker.tar.gz "https://github.com/jesseduffield/lazydocker/releases/latest/download/lazydocker_${LAZYDOCKER_VERSION}_Linux_${LAZYDOCKER_ARCH}.tar.gz"fi
 tar -xf lazydocker.tar.gz lazydocker
 sudo install lazydocker /usr/local/bin
 rm lazydocker.tar.gz lazydocker
