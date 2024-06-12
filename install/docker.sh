2f7222926270a5a7dc2919ad67e362807f56400c
Jeremy Daer <jeremy@37signals.com>
2024-06-11 17:52:59 -0700

    Correct merge from 88ffdebab1d535b20d22c6f392a644037fa92925

--- install/docker.sh
+++ install/docker.sh
@@ -12,10 +12,4 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 sudo usermod -aG docker ${USER}
 
 # Use local logging driver - it's more efficient and uses compression by default.
-echo '{"log-driver":"local","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
-
-DOCKER_COMPOSE_VERSION="2.27.0"
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -sSL https://github.com/docker/compose/releases/download/v$DOCKER_COMPOSE_VERSION/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+echo '{"log-driver":"local","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json > /dev/null
