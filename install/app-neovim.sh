a9e4340633ab39ecff0e9877b0681467aa6d4fba
Mats Estensen <mats.est@gmail.com>
2024-06-13 14:18:35 +0200

    fix: disable lazyvim plugin update notify
    Closes #115

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -2,6 +2,8 @@ sudo apt install -y neovim
 
 if [ ! -d "$HOME/.config/nvim" ]; then
 	git clone https://github.com/LazyVim/starter ~/.config/nvim
+	# Disable update notification popup in starter config
+	sed -i 's/checker = { enabled = true }/checker = { enabled = true, notify = false }/g' ~/.config/nvim/lua/config/lazy.lua
 	mkdir -p ~/.config/nvim/plugin/after
 	cp ~/.local/share/omakub/configs/neovim/transparency.lua ~/.config/nvim/plugin/after/
 	cp ~/.local/share/omakub/themes/neovim/tokyo-night.lua ~/.config/nvim/lua/plugins/theme.lua
