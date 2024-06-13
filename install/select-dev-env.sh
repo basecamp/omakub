dda59ae6982b4c8621699bbb8088864aa638062c
David Heinemeier Hansson <david@hey.com>
2024-06-13 21:12:51 +0200

    Include PHP as part of dev env setup

--- install/select-dev-env.sh
+++ install/select-dev-env.sh
@@ -1,5 +1,5 @@
 # Install default programming languages
-languages=$(gum choose "Ruby on Rails" "Node.js" "Go" "Python" "Java" "Elixir" --no-limit --selected "Ruby on Rails","Node.js" --height 8 --header "Select programming languages")
+languages=$(gum choose "Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Java" --no-limit --selected "Ruby on Rails","Node.js" --height 8 --header "Select programming languages")
 
 for language in $languages; do
 	case $language in
@@ -22,6 +22,13 @@ for language in $languages; do
 	Elixir)
 		mise use --global erlang@latest
 		mise use --global elixir@latest
+		;;
+	PHP)
+		sudo add-apt-repository -y ppa:ondrej/php
+		sudo apt -y install php8.3 php8.3-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
+		php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+		php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
+		rm composer-setup.php
 		;;
 	esac
 done
