9cec2222685d79543995b8141872ff56958f8c7e
David Heinemeier Hansson <david@hey.com>
2024-06-13 21:23:11 +0200

    Activate mise right away
    So we can rely on it for dev installs

--- install/mise.sh
+++ install/mise.sh
@@ -5,3 +5,4 @@ wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/
 echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
 sudo apt update
 sudo apt install -y mise
+eval "$(mise activate bash)"
