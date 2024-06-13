c93ae817a91c837c20853c9fc09c68c1a43f0519
Aurelien Navarre <anavarre@users.noreply.github.com>
2024-06-13 17:09:26 +0200

    Fix conflicts

--- install/flatpak.sh
+++ install/flatpak.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 sudo apt install -y flatpak
 sudo apt install -y gnome-software-plugin-flatpak
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
