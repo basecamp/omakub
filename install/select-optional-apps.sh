4a4f26dda2003240ab1857024273fd2b8c678bd5
David Heinemeier Hansson <david@hey.com>
2024-06-14 10:54:38 +0200

    Match height of options

--- install/select-optional-apps.sh
+++ install/select-optional-apps.sh
@@ -1,5 +1,5 @@
 # Install optional apps
-apps=$(gum choose "1password" "Spotify" "Zoom" "Dropbox" "Steam" --no-limit --selected "1password","Spotify","Zoom" --height 6 --header "Select optional apps")
+apps=$(gum choose "1password" "Spotify" "Zoom" "Dropbox" "Steam" --no-limit --selected "1password","Spotify","Zoom" --height 7 --header "Select optional apps")
 
 for app in $apps; do
 	source "$OMAKUB_PATH/install/optional/app-${app,,}.sh"
