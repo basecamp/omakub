d4aa5a3af68747e791afe04473178a83466ca5cd
Roeland <10061147+roelandmoors@users.noreply.github.com>
2024-06-15 20:35:52 +0200

    Fix neovim install
    The lib and share folder should also be copied. The binary is not enough

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -2,6 +2,8 @@ cd /tmp
 wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
 tar -xf nvim.tar.gz
 sudo install nvim-linux64/bin/nvim /usr/local/bin/nvim
+sudo cp -R nvim-linux64/lib /usr/local/
+sudo cp -R nvim-linux64/share /usr/local/
 rm -rf nvim-linux64 nvim.tar.gz
 cd -
 
