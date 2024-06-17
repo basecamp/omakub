4a188f7cdf7f0706bde3ab78a658c963c2831c66
Remy Wang <remy.wang727@gmail.com>
2024-06-17 17:42:55 -0400

    fix: install alacarity on Ubuntu 22.04

--- install/app-alacritty.sh
+++ install/app-alacritty.sh
@@ -1,4 +1,8 @@
-sudo apt install -y alacritty
+# sudo apt install -y alacritty
+sudo apt update && sudo apt upgrade
+sudo add-apt-repository ppa:aslatter/ppa -y
+sudo apt install alacritty -y
+
 mkdir -p ~/.config/alacritty
 cp ~/.local/share/omakub/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
 cp ~/.local/share/omakub/configs/alacritty/theme.toml ~/.config/alacritty/theme.toml
