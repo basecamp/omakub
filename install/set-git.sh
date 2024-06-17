686d737ada2b4cdb716817ee0aee1d0b008f2e82
Remy Wang <remy.wang727@gmail.com>
2024-06-17 17:20:39 -0400

    fix: update VSCode settings and set git

--- install/set-git.sh
+++ install/set-git.sh
@@ -1,6 +1,4 @@
 # Set common git aliases
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.st status
 git config --global pull.rebase true
+git config --global user.name "Remy Wang"
+git config --global user.email "remy.wang727@gmail.com"
