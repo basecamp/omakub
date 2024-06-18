5189e119e407524c159aa5974a7484dca3d2fdb3
Remy Wang <remy.wang727@gmail.com>
2024-06-18 14:08:53 -0400

    feat: install PostgreSQL 16 and Redis

--- install/libraries.sh
+++ install/libraries.sh
@@ -1,5 +1,18 @@
 sudo apt install -y \
-	build-essential pkg-config autoconf bison rustc cargo clang \
+	build-essential pkg-config autoconf bison rustc cargo clang gnupg2 \
 	libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
 	libvips imagemagick libmagickwand-dev mupdf mupdf-tools \
-	redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev
+	redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev libpq-dev
+
+# Install PostgreSQL 16
+# sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+# curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+# sudo apt update
+# sudo apt install -y postgresql-16 postgresql-contrib-16
+
+# Install Redis
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+sudo apt-get update
+sudo apt-get install -y redis
+sudo systemctl enable redis-server
