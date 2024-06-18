a0b2e6a44d060bbce74526093a2501a77325aa38
Collin Barnwell <barnwell.collin@gmail.com>
2024-06-18 10:34:05 -0700

    Update app-alacritty.sh

--- install/app-alacritty.sh
+++ install/app-alacritty.sh
@@ -3,4 +3,4 @@ mkdir -p ~/.config/alacritty
 cp ~/.local/share/omakub/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
 cp ~/.local/share/omakub/configs/alacritty/theme.toml ~/.config/alacritty/theme.toml
 cp ~/.local/share/omakub/configs/alacritty/font.toml ~/.config/alacritty/font.toml
-echo 1 | sudo update-alternatives --config x-terminal-emulator
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/alacritty 50
