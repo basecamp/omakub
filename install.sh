e03ee2ef4c02acea4b8a20b2e6bcdfb5393bc604
javajosh <javajosh@gmail.com>
2024-07-03 12:18:21 -0400

    Update install.sh to abort installation on non ubuntu and non ubuntu 24 systems
    Omakub is verified to be incompatible with Ubuntu 22, and in fact has disasterous side-effects. The primary install script now checks distribution name and version and aborts when either is incorrect. This change is compatible with future versions of Ubuntu.

--- install.sh
+++ install.sh
@@ -1,6 +1,68 @@
 # Exit immediately if a command exits with a non-zero status
 # set -e
 
+# Abort installation if the system is either not Ubuntu or not Ubuntu 24.04
+# Function to compare version numbers
+version_compare() {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
+
+# Check if the system is Ubuntu
+if [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    if [ "$DISTRIB_ID" != "Ubuntu" ]; then
+        echo "This script is intended for Ubuntu systems only."
+        exit 1
+    fi
+else
+    echo "This script is intended for Ubuntu systems only."
+    exit 1
+fi
+
+# Get Ubuntu version
+ubuntu_version=$(lsb_release -rs)
+
+# Minimum required version
+min_version="24.04"
+
+# Compare versions
+version_compare $ubuntu_version $min_version
+result=$?
+
+if [ $result -eq 2 ]; then
+    echo "Error: Ubuntu version $ubuntu_version is lower than the required version $min_version"
+    exit 1
+else
+    echo "Success: Ubuntu version $ubuntu_version meets the minimum requirement of $min_version"
+    exit 0
+fi
+
+
 # Desktop software and tweaks will only be installed if we're running Gnome
 RUNNING_GNOME=$([[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]] && echo true || echo false)
 
@@ -29,3 +91,4 @@ if $RUNNING_GNOME; then
 	gsettings set org.gnome.desktop.screensaver lock-enabled true
 	gsettings set org.gnome.desktop.session idle-delay 300
 fi
+
