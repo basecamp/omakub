6b40bb066970f01ee6db16665df1778221d75302
David Heinemeier Hansson <david@hey.com>
2024-07-03 17:41:08 +0200

    Missing space

--- install/desktop/app-alacritty.sh
+++ install/desktop/app-alacritty.sh
@@ -4,4 +4,4 @@ mkdir -p ~/.config/alacritty
 cp ~/.local/share/omakub/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
 cp ~/.local/share/omakub/themes/tokyo-night/alacritty.toml ~/.config/alacritty/theme.toml
 cp ~/.local/share/omakub/configs/alacritty/fonts/CaskaydiaMono.toml ~/.config/alacritty/font.toml
-cp ~/.local/share/omakub/configs/alacritty/font-size.toml~/.config/alacritty/font-size.toml
+cp ~/.local/share/omakub/configs/alacritty/font-size.toml ~/.config/alacritty/font-size.toml
