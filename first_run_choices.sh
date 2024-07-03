85e2120bf1b3e30708c7a10940e9a2a7ae1af027
David Heinemeier Hansson <david@hey.com>
2024-07-03 14:16:52 +0200

    Make choices up front

---
+++ first_run_choices.sh
@@ -0,0 +1,11 @@
+OPTIONAL_APPS=("1password" "Spotify" "Zoom" "Dropbox")
+DEFAULT_OPTIONAL_APPS='1password,Spotify,Zoom'
+export OMAKUB_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
+
+AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Rust" "Java")
+SELECTED_LANGUAGES="Ruby on Rails","Node.js"
+export OMAKUB_FIRST_RUN_LANGUAGES=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --selected "$SELECTED_LANGUAGES" --height 10 --header "Select programming languages")
+
+AVAILABLE_DBS=("MySQL" "Redis" "PostgreSQL")
+SELECTED_DBS="MySQL,Redis"
+export OMAKUB_FIRST_RUN_DBS=$(gum choose "${AVAILABLE_DBS[@]}" --no-limit --selected "$SELECTED_DBS" --height 5 --header "Select databases (runs in Docker)")
