fb9699dd279f4807cafd47612433559360829163
David Heinemeier Hansson <david@hey.com>
2024-07-03 19:04:02 +0200

    Reveal week numbers in the Gnome calendar
    Closes #157

--- install/desktop/set-gnome-settings.sh
+++ install/desktop/set-gnome-settings.sh
@@ -3,3 +3,6 @@ gsettings set org.gnome.mutter center-new-windows true
 
 # Set Cascadia Mono as the default monospace font
 gsettings set org.gnome.desktop.interface monospace-font-name 'CaskaydiaMono Nerd Font 10'
+
+# Reveal week numbers in the Gnome calendar
+gsettings set org.gnome.desktop.calendar show-weekdate true
