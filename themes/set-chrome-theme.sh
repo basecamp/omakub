156bb0ce0a53f087c392a6b0e94b26323f095503
David Heinemeier Hansson <david@hey.com>
2024-07-03 14:16:14 +0200

    Explain even though not currently in use

--- themes/set-chrome-theme.sh
+++ themes/set-chrome-theme.sh
@@ -1,5 +1,6 @@
-# Chrome theme update
-if gum confirm "Do you want to update Chrome theme? If yes, Chrome will be restarted!"; then
+# Updating the Chrome settings require that we first quit the application, since settings aren't picked up live,
+# and they're overwritten on exit.
+if gum confirm "Update Chrome theme? If yes, Chrome (and all Chrome-based apps) will closed!"; then
 	pkill -f chrome
 
 	while pgrep -f chrome >/dev/null; do
@@ -11,6 +12,4 @@ if gum confirm "Do you want to update Chrome theme? If yes, Chrome will be resta
 	jq ".browser.theme.color_scheme = $CHROME_COLOR_SCHEME | .browser.theme.color_variant = $CHROME_COLOR_VARIANT | .browser.theme.user_color = $CHROME_COLOR" Preferences >tmp.json && mv tmp.json Preferences
 	jq ".ntp += {\"custom_background_dict\":{\"background_url\": \"https://github.com/basecamp/omakub/blob/master/themes/$CHROME_BACKGROUND?raw=true\"}}" Preferences >tmp.json && mv tmp.json Preferences
 	cd ~
-
-	google-chrome >/dev/null 2>&1 &
 fi
