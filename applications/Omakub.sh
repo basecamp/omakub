7dab0029491254771a520492a8ebe9f450621914
David Heinemeier Hansson <david@hey.com>
2024-07-03 14:15:16 +0200

    New desktop applications

--- applications/Omakub.sh
+++ applications/Omakub.sh
@@ -3,10 +3,10 @@ cat <<EOF >~/.local/share/applications/Omakub.desktop
 Version=1.0
 Name=Omakub
 Comment=Omakub Controls
-Exec=alacritty --config-file /home/$USER/.local/share/omakub/defaults/alacritty-pane.toml -e omakub
+Exec=alacritty --config-file /home/$USER/.local/share/omakub/defaults/alacritty/pane.toml -e omakub
 Terminal=false
 Type=Application
 Icon=/home/$USER/.local/share/omakub/applications/icons/Omakub.png
 Categories=GTK;
-StartupNotify=true
+StartupNotify=false
 EOF
