c06a0b041a19b7e55854ffaebc5df7c38cc1c6dd
David Heinemeier Hansson <david@hey.com>
2024-07-03 14:16:41 +0200

    Have boot live here rather than in site repo

---
+++ boot.sh
@@ -0,0 +1,23 @@
+set -e
+
+ascii_art='________                  __        ___.
+\_____  \   _____ _____  |  | ____ _\_ |__
+ /   |   \ /     \\__   \ |  |/ /  |  \ __ \
+/    |    \  Y Y  \/ __ \|    <|  |  / \_\ \
+\_______  /__|_|  (____  /__|_ \____/|___  /
+        \/      \/     \/     \/         \/
+'
+
+echo -e "$ascii_art"
+echo "=> Omakub is for fresh Ubuntu 24.04 installations only!"
+echo -e "\nBegin installation (or abort with ctrl+c)..."
+
+sudo apt-get update >/dev/null
+sudo apt-get install -y git >/dev/null
+
+echo "Cloning stable Omakub..."
+rm -rf ~/.local/share/omakub
+git clone -b stable https://github.com/basecamp/omakub.git ~/.local/share/omakub >/dev/null
+
+echo "Installation starting..."
+source ~/.local/share/omakub/install.sh
