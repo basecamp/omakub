f75254c039f3414529b33128233ec8531c1d0a54
Alan Maciel <alan.maciel.salcedo@gmail.com>
2024-07-06 14:29:24 -0600

    Remove some libs

--- install/desktop/optional/app-doom-emacs.sh
+++ install/desktop/optional/app-doom-emacs.sh
@@ -1,5 +1,3 @@
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y emacs cmake libtool-bin
+sudo apt install -y emacs
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom install
