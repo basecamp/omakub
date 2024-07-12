cf10044c2dc60140582057385f37c1ef3abc4b18
Albert Skonieczny <albertskonieczny@gmail.com>
2024-07-12 22:26:15 +0200

    Fix eza installation

--- install/terminal/apps-terminal.sh
+++ install/terminal/apps-terminal.sh
@@ -1,1 +1,9 @@
-sudo apt install -y fzf ripgrep bat eza zoxide plocate btop apache2-utils fd-find
+sudo apt install -y fzf ripgrep bat zoxide plocate btop apache2-utils fd-find
+
+#Install eza
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
