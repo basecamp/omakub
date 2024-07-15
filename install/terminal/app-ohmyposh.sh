f0771a0aba35788d1caf7aabc10bbf8e9900da96
Erik Giovani <46170949+ErikGIovani@users.noreply.github.com>
2024-07-15 01:52:22 -0600

    Oh My Posh added

--- install/terminal/app-ohmyposh.sh
+++ install/terminal/app-ohmyposh.sh
@@ -1,3 +1,4 @@
+mkdir -p ~/.local/bin
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
 mkdir -p ~/.config/oh-my-posh/themes
 cp ~/.local/share/omakub/themes/tokyo-night/ohmyposh.json ~/.config/oh-my-posh/themes/theme.json
