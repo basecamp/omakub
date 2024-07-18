1df9bfeabb7dd5fc4f0cf01c6ee72f5819220e1d
Ibrahim <8837895+IbrahimTanyalcin@users.noreply.github.com>
2024-07-18 19:52:44 +0000

    add comments to get_arch

--- get_arch.sh
+++ get_arch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e;
 
+#assoc array to list app specific arch aliases
 declare -A arch_config=(
     ["gum.x86_64"]="amd64"
     ["gum.aarch64"]="arm64"
@@ -16,6 +17,7 @@ declare -A arch_config=(
     ["lazydocker.aarch64"]="arm64"
 )
 
+#a getter func to lookup into above assos array. Unfound keys/default value is what is returned from lscpu
 get_arch_config() {
     local arch="${1}"
     local prog="${2:-"default"}"
@@ -24,6 +26,8 @@ get_arch_config() {
     echo -n "$archAlias"
 }
 
+# Usage:
+# echo "$(get_arch "chrome")" //for aarch64, returns arm64
 get_arch() {
     local prog="$1"
     local ARCH=$(lscpu | awk -F: 'NR==1 {print $2}' | xargs)
