03dfdd72598998f169e0c7dc3bb8247bc3332c39
Ibrahim <8837895+IbrahimTanyalcin@users.noreply.github.com>
2024-07-18 19:34:56 +0000

    add arch alias for gum into get_arch.sh

--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -1,7 +1,7 @@
 # Gum is used for the Omakub commands for tailoring Omakub after the initial install
 cd /tmp
 source ~/.local/share/omakub/get_arch.sh
-ARCH=$(get_arch)
+ARCH=$(get_arch "gum")
 GUM_VERSION="0.14.3" # Use known good version
 wget -qO gum.deb "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_${ARCH}.deb"
 sudo apt-get install -y ./gum.deb
