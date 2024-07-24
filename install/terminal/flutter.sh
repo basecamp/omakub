2ecbfb2a1d3679a71948f04d7e50e2be1a6d664b
Cillian Myles <myles.cillian@gmail.com>
2024-07-24 23:23:22 +0100

    chore: simplify as much as possible

--- install/terminal/flutter.sh
+++ install/terminal/flutter.sh
@@ -1,13 +1,9 @@
-#!/bin/
+#!/bin/bash
 
 sudo snap install flutter --classic
 sudo snap alias flutter.dart dart
 flutter --version
-
-echo "export PATH=\$PATH:/opt/flutter/bin" >> ~/.bashrc
-source ~/.bashrc
 flutter doctor
 
-# Install VS Code extensions
-code --install-extension dart-code.dart-code --force
+# Install VS Code extension
 code --install-extension dart-code.flutter --force
--- uninstall/flutter.sh
+++ uninstall/flutter.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 sudo snap remove flutter
-sed -i '/\/opt\/flutter\/bin/d' ~/.bashrc
-source ~/.bashrc
 
-# Uninstall VS Code extensions
-code --uninstall-extension dart-code.dart-code
+# Uninstall VS Code extension
 code --uninstall-extension dart-code.flutter
