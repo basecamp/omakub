b0844e8a7b506e2bb1686d34e9de2d705bd2c466
David Heinemeier Hansson <david@hey.com>
2024-07-27 10:51:21 -0700

    Fix indention

--- install.sh
+++ install.sh
@@ -8,27 +8,27 @@ RUNNING_GNOME=$([[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]] && echo true || echo f
 source ~/.local/share/omakub/check-version.sh
 
 if $RUNNING_GNOME; then
-	# Ensure computer doesn't go to sleep or lock while installing
-	gsettings set org.gnome.desktop.screensaver lock-enabled false
-	gsettings set org.gnome.desktop.session idle-delay 0
+  # Ensure computer doesn't go to sleep or lock while installing
+  gsettings set org.gnome.desktop.screensaver lock-enabled false
+  gsettings set org.gnome.desktop.session idle-delay 0
 
-	echo "Get ready to make a few choices..."
-	source ~/.local/share/omakub/install/terminal/required/app-gum.sh >/dev/null
+  echo "Get ready to make a few choices..."
+  source ~/.local/share/omakub/install/terminal/required/app-gum.sh >/dev/null
   source ~/.local/share/omakub/install/first_run_choices.sh
 
-	echo "Installing terminal and desktop tools.."
+  echo "Installing terminal and desktop tools.."
 else
-	echo "Only installing terminal tools..."
+  echo "Only installing terminal tools..."
 fi
 
 # Install terminal tools
 source ~/.local/share/omakub/install/terminal.sh
 
 if $RUNNING_GNOME; then
-	# Install desktop tools and tweaks
-	source ~/.local/share/omakub/install/desktop.sh
+  # Install desktop tools and tweaks
+  source ~/.local/share/omakub/install/desktop.sh
 
-	# Revert to normal idle and lock settings
-	gsettings set org.gnome.desktop.screensaver lock-enabled true
-	gsettings set org.gnome.desktop.session idle-delay 300
+  # Revert to normal idle and lock settings
+  gsettings set org.gnome.desktop.screensaver lock-enabled true
+  gsettings set org.gnome.desktop.session idle-delay 300
 fi
