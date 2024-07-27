e94dae6ad4cf795e7f0e046e78fdf4fef1fc1f32
David Heinemeier Hansson <david@hey.com>
2024-07-27 07:51:52 -0700

    Add postgresql client libraries
    Closes #217

--- install/terminal/libraries.sh
+++ install/terminal/libraries.sh
@@ -2,4 +2,4 @@ sudo apt install -y \
   build-essential pkg-config autoconf bison clang rustc \
   libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
   libvips imagemagick libmagickwand-dev mupdf mupdf-tools \
-  redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev
+  redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common
