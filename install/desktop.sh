0e2b610160381894b645a8ea0154a440761141bc
David Heinemeier Hansson <david@hey.com>
2024-08-02 11:03:42 -0700

    Switch from logout to reboot
    Needed to pickup the group changes required for Docker

--- install/desktop.sh
+++ install/desktop.sh
@@ -10,4 +10,4 @@ gsettings set org.gnome.desktop.screensaver lock-enabled true
 gsettings set org.gnome.desktop.session idle-delay 300
 
 # Logout to pickup changes
-gum confirm "Ready to logout for all settings to take effect?" && gnome-session-quit --logout --no-prompt
+gum confirm "Ready to reboot for all settings to take effect?" && sudo reboot
