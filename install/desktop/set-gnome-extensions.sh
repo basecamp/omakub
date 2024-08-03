e982dc5454ff927fe51d3074a2b66741ad2a6287
Luca Pattocchio <pattocchioluca@gmail.com>
2024-08-03 14:19:17 +0200

    fix extension ref

--- install/desktop/set-gnome-extensions.sh
+++ install/desktop/set-gnome-extensions.sh
@@ -16,7 +16,7 @@ gext install just-perfection-desktop@just-perfection
 gext install blur-my-shell@aunetx
 gext install space-bar@luchrioh
 gext install undecorate@sun.wxg@gmail.com
-gext install tophat@fflewddur
+gext install tophat@fflewddur.github.io
 gext install AlphabeticalAppGrid@stuarthayhurst
 
 # Compile gsettings schemas in order to be able to set them
