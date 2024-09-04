fcae05ac604a989aec0085cf5b7a9068122ce598
Rohit Das <43847374+therohitdas@users.noreply.github.com>
2024-09-04 18:39:09 +0530

    Add ARCH CHECK to download appropriate packages

--- install/desktop/app-signal.sh
+++ install/desktop/app-signal.sh
@@ -1,3 +1,6 @@
+if [ "$(uname -m)" = "aarch64" ]; then
+    sudo snap install signal-desktop
+else
 wget -qO- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor >signal-desktop-keyring.gpg
 cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg >/dev/null
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |
@@ -5,3 +8,4 @@ echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] 
 rm signal-desktop-keyring.gpg
 sudo apt update
 sudo apt install -y signal-desktop
+fi
