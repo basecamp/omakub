af0628e69962eb9ba4389c4987a6dfefb6d7fb50
Rohit Das <43847374+therohitdas@users.noreply.github.com>
2024-09-04 21:38:57 +0530

    Fixed bash syntax

--- install/terminal/app-zellij.sh
+++ install/terminal/app-zellij.sh
@@ -1,5 +1,5 @@
 cd /tmp
-TARGET_ARCH=$(["$(dpkg --print-architecture)" = "arm64"] && echo "aarch64" || echo "x86_64")
+TARGET_ARCH=$([ "$(dpkg --print-architecture)" = "arm64" ] && echo "aarch64" || echo "x86_64")
 wget -O zellij.tar.gz "https://github.com/zellij-org/zellij/releases/latest/download/zellij-${TARGET_ARCH}-unknown-linux-musl.tar.gz"
 tar -xf zellij.tar.gz zellij
 sudo install zellij /usr/local/bin
