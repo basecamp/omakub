b96606bd38d38e9ec80edb10070d871b5b0b0fd1
Rohit Das <43847374+therohitdas@users.noreply.github.com>
2024-09-04 19:36:39 +0530

    Set chromium as default browser

--- install/desktop/app-chrome.sh
+++ install/desktop/app-chrome.sh
@@ -1,6 +1,7 @@
 # Browse the web with the most popular browser. See https://www.google.com/chrome/
 if [ "$(dpkg --print-architecture)" = "arm64" ]; then
     sudo apt install -y chromium-browser
+    xdg-settings set default-web-browser chromium-browser.desktop
 else
     cd /tmp
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
