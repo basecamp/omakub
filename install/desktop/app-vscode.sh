8e5ae607937d1a5a6603afebfd06ba4b5e4bb07a
Rohit Das <43847374+therohitdas@users.noreply.github.com>
2024-09-04 18:26:17 +0530

    Add ARCH CHECK to download appropriate packages

--- install/desktop/app-vscode.sh
+++ install/desktop/app-vscode.sh
@@ -1,5 +1,6 @@
 cd /tmp
-wget -O code.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
+TARGET_ARCH=$(["$(dpkg --print-architecture)" = "arm64"] && echo "arm64" || echo "x64")
+wget -O code.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-${TARGET_ARCH}'
 sudo apt install -y ./code.deb
 rm code.deb
 cd -
--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -1,7 +1,8 @@
 # Gum is used for the Omakub commands for tailoring Omakub after the initial install
 cd /tmp
 GUM_VERSION="0.14.3" # Use known good version
-wget -qO gum.deb "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_amd64.deb"
+TARGET_ARCH=$(dpkg --print-architecture)
+wget -qO gum.deb "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_${TARGET_ARCH}.deb"
 sudo apt-get install -y ./gum.deb
 rm gum.deb
 cd -
