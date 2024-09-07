1c4dcf73d900f839d68964797a1ebf4ba6332655
David Heinemeier Hansson <david@hey.com>
2024-09-07 15:17:57 -0700

    Install code directly to avoid user interaction for code signing

--- install/desktop/app-vscode.sh
+++ install/desktop/app-vscode.sh
@@ -1,8 +1,11 @@
 cd /tmp
-wget -O code.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
-sudo apt install -y ./code.deb
-rm code.deb
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
+rm -f packages.microsoft.gpg
 cd -
+
+sudo apt install code
 
 mkdir -p ~/.config/Code/User
 cp ~/.local/share/omakub/configs/vscode.json ~/.config/Code/User/settings.json
