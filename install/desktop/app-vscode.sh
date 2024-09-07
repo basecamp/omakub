bbbe6763fc2003f7db5cd705c496d0404df16eac
David Heinemeier Hansson <david@hey.com>
2024-09-07 15:19:35 -0700

    Update first

--- install/desktop/app-vscode.sh
+++ install/desktop/app-vscode.sh
@@ -5,7 +5,8 @@ echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft
 rm -f packages.microsoft.gpg
 cd -
 
-sudo apt install code
+sudo apt update -y
+sudo apt install -y code
 
 mkdir -p ~/.config/Code/User
 cp ~/.local/share/omakub/configs/vscode.json ~/.config/Code/User/settings.json
