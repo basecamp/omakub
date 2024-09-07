c4453baeae0b69ec8405fc108d61e79018a027c2
1gn0r4nd <53620455+1gn0r4nd@users.noreply.github.com>
2024-09-07 04:55:28 -0300

    Update app-localsend.sh
    use flatpak instead of getting deb file

--- install/desktop/app-localsend.sh
+++ install/desktop/app-localsend.sh
@@ -1,6 +1,1 @@
-cd /tmp
-LOCALSEND_VERSION=$(curl -s "https://api.github.com/repos/localsend/localsend/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-wget -O localsend.deb "https://github.com/localsend/localsend/releases/latest/download/LocalSend-${LOCALSEND_VERSION}-linux-x86-64.deb"
-sudo apt install -y ./localsend.deb
-rm localsend.deb
-cd -
+flatpak install flathub org.localsend.localsend_app
