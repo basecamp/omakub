05960baee4d6c70c340fb27b68c279e7474073f2
David Heinemeier Hansson <david@hey.com>
2024-09-07 21:09:28 -0700

    Remove ufw until we can sort it out proper
    Not working as intended at the moment

--- migrations/1724276595.sh
+++
@@ -1,4 +0,0 @@
-echo "Hardening docker..."
-echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5", "iptables":false}}' | sudo tee /etc/docker/daemon.json
-echo "Enabling ufw firewall"
-source $OMAKUB_PATH/install/ufw.sh
