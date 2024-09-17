303dd2f4bedebb93a4f7d7b332e21b350a7f52b0
SagaFish <39843121+SagaciousFish@users.noreply.github.com>
2024-09-17 10:13:53 +0800

    Ensure dependencies required by the terminal install scripts are fulfilled before calling

--- install/terminal.sh
+++ install/terminal.sh
@@ -3,5 +3,8 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y curl git unzip
 
+# Run required installers first
+for installer in ~/.local/share/omakub/install/terminal/required/*.sh; do source $installer; done
+
 # Run terminal installers
 for installer in ~/.local/share/omakub/install/terminal/*.sh; do source $installer; done
