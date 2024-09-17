be6bfef8d913fec53272c6733f6bc226bf54e95f
SagaFish <39843121+SagaciousFish@users.noreply.github.com>
2024-09-17 14:41:02 +0800

    Update for correct fix timing for bash-completion

--- install/terminal/apps-terminal.sh
+++ install/terminal/apps-terminal.sh
@@ -1,1 +1,1 @@
-sudo apt install -y bash-completion ripgrep bat eza zoxide plocate btop apache2-utils fd-find tldr
+sudo apt install -y ripgrep bat eza zoxide plocate btop apache2-utils fd-find tldr
