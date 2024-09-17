875a4e61f0d342bc0611d596413521d4df05ded5
SagaFish <39843121+SagaciousFish@users.noreply.github.com>
2024-09-17 10:29:15 +0800

    Replace dependency on `bc` with `python3`

--- install/check-version.sh
+++ install/check-version.sh
@@ -9,7 +9,7 @@ fi
 . /etc/os-release
 
 # Check if running on Ubuntu 24.04 or higher
-if [ "$ID" != "ubuntu" ] || [ $(echo "$VERSION_ID >= 24.04" | bc) != 1 ]; then
+if [ "$ID" != "ubuntu" ] || [ $(python3 -c "print(float('$VERSION_ID') >= 24.04)") != 1 ]; then
     echo "$(tput setaf 1)Error: OS requirement not met"
     echo "You are currently running: $ID $VERSION_ID"
     echo "OS required: ubuntu 24.04 or higher"
