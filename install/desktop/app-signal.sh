e630faea382e8acec067a871ce66c52d6ce25741
Adam Brown <avb@dicebag.com>
2024-10-05 21:30:00 -0500

    Add support for arm64 where available

--- install/desktop/app-signal.sh
+++ install/desktop/app-signal.sh
@@ -1,7 +1,11 @@
-wget -qO- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor >signal-desktop-keyring.gpg
-cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg >/dev/null
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |
-	sudo tee /etc/apt/sources.list.d/signal-xenial.list
-rm signal-desktop-keyring.gpg
-sudo apt update
-sudo apt install -y signal-desktop
+if [ "$dpkg --print-architecture" = "arm64" ]; then
+	sudo snap install signal-desktop
+fi
+	wget -qO- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor >signal-desktop-keyring.gpg
+	cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg >/dev/null
+	echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |
+		sudo tee /etc/apt/sources.list.d/signal-xenial.list
+	rm signal-desktop-keyring.gpg
+	sudo apt update
+	sudo apt install -y signal-desktop
+fi
