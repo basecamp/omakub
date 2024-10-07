4910f1089a35183a997ff63680f814d79409ca06
asmrtfm <asmrtfm@gmail.com>
2024-10-07 00:10:47 -0400

    remove redundant idle and lock settings changes from install/desktop.sh, move the restoring of said settings into install.sh, and attempt to respect previously configured settings

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ RUNNING_GNOME=$([[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]] && echo true || echo f
 source ~/.local/share/omakub/install/check-version.sh
 
 if $RUNNING_GNOME; then
+  # Try to respect any previously configured settings
+  previous_screensaver_lock=$(gsettings get org.gnome.desktop.screensaver lock-enabled) || true
+  previous_idle_delay=$(gsettings get org.gnome.desktop.session idle-delay | cut -d\  -f2) || true
+
   # Ensure computer doesn't go to sleep or lock while installing
   gsettings set org.gnome.desktop.screensaver lock-enabled false
   gsettings set org.gnome.desktop.session idle-delay 0
@@ -28,7 +32,7 @@ if $RUNNING_GNOME; then
   # Install desktop tools and tweaks
   source ~/.local/share/omakub/install/desktop.sh
 
-  # Revert to normal idle and lock settings
-  gsettings set org.gnome.desktop.screensaver lock-enabled true
-  gsettings set org.gnome.desktop.session idle-delay 300
+  # Restore idle and lock settings
+  gsettings set org.gnome.desktop.screensaver lock-enabled ${previous_screensaver_lock:-true}
+  gsettings set org.gnome.desktop.session idle-delay ${previous_idle_delay:-300}
 fi
