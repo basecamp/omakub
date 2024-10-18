1995664275ece8a09575046304eff31daa6ea523
Justin Horner <contact@justinhhorner.com>
2024-10-18 00:06:56 -0400

    Add emoji extension for Ulauncher

---
+++ migrations/1729224919.sh
@@ -0,0 +1,4 @@
+# Add Ulauncher emoji extension
+if [ ! -d "$HOME/.local/share/ulauncher/extensions/com.github.ulauncher.ulauncher-emoji/" ]; then
+  git clone https://github.com/Ulauncher/ulauncher-emoji.git $HOME/.local/share/ulauncher/extensions/com.github.ulauncher.ulauncher-emoji/
+fi
