38c724cd017f83cb3cbe46e0752b892af3e3137f
Ninad Sachania <ninad.sachania@gmail.com>
2024-10-19 13:30:03 +0530

    Convert indentation to tabs

--- bin/omakub-sub/migrate.sh
+++ bin/omakub-sub/migrate.sh
@@ -3,13 +3,13 @@ last_updated_at=$(git log -1 --format=%cd --date=unix)
 git pull
 
 for file in $OMAKUB_PATH/migrations/*.sh; do
-  filename=$(basename "$file")
-  migrate_at="${filename%.sh}"
+	filename=$(basename "$file")
+	migrate_at="${filename%.sh}"
 
-  if [ $migrate_at -gt $last_updated_at ]; then
-    echo "Running migration for $migrate_at"
-    source $file
-  fi
+	if [ $migrate_at -gt $last_updated_at ]; then
+		echo "Running migration for $migrate_at"
+		source $file
+	fi
 done
 
 cd -
