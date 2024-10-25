177ef9a6751d79613491335c887574776a88ce47
Matteo Ferrini <mimmi.ferrini@gmail.com>
2024-10-25 21:46:39 +0200

    Remove check

--- install.sh
+++ install.sh
@@ -4,9 +4,6 @@ set -e
 # Desktop software and tweaks will only be installed if we're running Gnome
 RUNNING_GNOME=$([[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]] && echo true || echo false)
 
-# Check the distribution name and version and abort if incompatible
-source ~/.local/share/omakub/install/check-version.sh
-
 if $RUNNING_GNOME; then
   # Ensure computer doesn't go to sleep or lock while installing
   gsettings set org.gnome.desktop.screensaver lock-enabled false
