dbb08914ad9c6a2d0b08c1c4d91c71360e8054fa
Joel Enriquez <jlenriquez@gmail.com>
2024-10-27 16:20:52 +0900

    Added new profile for LENOVO laptop using 2.2K display.

--- install/desktop/set-framework-text-scaling.sh
+++ install/desktop/set-framework-text-scaling.sh
@@ -1,8 +1,14 @@
 COMPUTER_MAKER=$(sudo dmidecode -t system | grep 'Manufacturer:' | awk '{print $2}')
 SCREEN_RESOLUTION=$(xrandr | grep '*+' | awk '{print $1}')
 
+# Framework
 if [ "$COMPUTER_MAKER" == "Framework" ] && [ "$SCREEN_RESOLUTION" == "2256x1504" ]; then
   gsettings set org.gnome.desktop.interface text-scaling-factor 0.8
   gsettings set org.gnome.desktop.interface cursor-size 16
   sed -i "s/size = 9/size = 7/g" ~/.config/alacritty/font-size.toml
+# Lenovo
+elif [ "$COMPUTER_MAKER" == "LENOVO" ] && [ "$SCREEN_RESOLUTION" == "2240x1400" ]; then
+  gsettings set org.gnome.desktop.interface text-scaling-factor 0.75
+  gsettings set org.gnome.desktop.interface cursor-size 16
+  sed -i "s/size = 9/size = 8.25/g" ~/.config/alacritty/font-size.toml
 fi
