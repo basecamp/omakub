3d68eceae90e80731346373e0f70515165b0b2a0
amarqs182 <amarqs182@gmail.com>
2024-11-11 08:16:06 -0300

    Update installation scripts and documentation for improved user experience and Fedora compatibility

--- install/desktop/set-gnome-extensions.sh
+++ install/desktop/set-gnome-extensions.sh
@@ -1,11 +1,11 @@
 sudo apt install -y gnome-shell-extension-manager pipx
 pipx install gnome-extensions-cli --system-site-packages
 
-# Turn off default Ubuntu extensions
-gnome-extensions disable tiling-assistant@ubuntu.com
-gnome-extensions disable ubuntu-appindicators@ubuntu.com
-gnome-extensions disable ubuntu-dock@ubuntu.com
-gnome-extensions disable ding@rastersoft.com
+## Turn off default Ubuntu extensions
+#gnome-extensions disable tiling-assistant@ubuntu.com
+#gnome-extensions disable ubuntu-appindicators@ubuntu.com
+#gnome-extensions disable ubuntu-dock@ubuntu.com
+#gnome-extensions disable ding@rastersoft.com
 
 # Pause to assure user is ready to accept confirmations
 gum confirm "To install Gnome extensions, you need to accept some confirmations. Are you ready?"
@@ -72,3 +72,4 @@ gsettings set org.gnome.shell.extensions.tophat network-usage-unit bits
 
 # Configure AlphabeticalAppGrid
 gsettings set org.gnome.shell.extensions.alphabetical-app-grid folder-order-position 'end'
+
--- install/terminal/docker.sh
+++ install/terminal/docker.sh
@@ -1,15 +1,17 @@
-# Add the official Docker repo
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo wget -qO /etc/apt/keyrings/docker.asc https://download.docker.com/linux/ubuntu/gpg
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
+# Instalar o plugin DNF e configurar o repositório Docker
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf update
 
-# Install Docker engine and standard plugins
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+# Instalar Docker Engine e plugins padrão
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
 
-# Give this user privileged Docker access
+# Iniciar e habilitar o serviço Docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Dar acesso privilegiado do Docker para este usuário
 sudo usermod -aG docker ${USER}
 
-# Limit log size to avoid running out of disk
+# Limitar tamanho dos logs para evitar encher o disco
 echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
