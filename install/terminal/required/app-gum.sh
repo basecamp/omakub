ca04d84d39c73e176b240efa643257bf425d5139
amarqs182 <amarqs182@gmail.com>
2024-11-11 08:48:13 -0300

    Update Gum version to 0.14.5 in installation script

--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -1,7 +1,7 @@
 # Gum is used for the omakfed commands for tailoring omakfed after the initial install
 cd /tmp
-GUM_VERSION="0.14.3" # Use known good version
-wget -qO gum.rpm "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_x86_64.rpm"
+GUM_VERSION="0.14.5" # Use known good version
+wget -qO gum.rpm "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum-${GUM_VERSION}-1.x86_64.rpm"
 sudo dnf install -y ./gum.rpm
 rm gum.rpm
 cd -
