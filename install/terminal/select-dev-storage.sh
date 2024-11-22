8a6a270edc110539316adad08cbbe730820a1f28
Luke Hsiao <luke@hsiao.dev>
2024-11-22 02:11:37 -0700

    fix(install/terminal): specify docker registry
    Bluefin has 3 registries set for unqualified search.

--- install/terminal/select-dev-storage.sh
+++ install/terminal/select-dev-storage.sh
@@ -6,17 +6,18 @@ else
 	dbs=$(gum choose "${AVAILABLE_DBS[@]}" --no-limit --height 5 --header "Select databases (runs in Docker)")
 fi
 
+
 if [[ -n "$dbs" ]]; then
 	for db in $dbs; do
 		case $db in
 		MySQL)
-			podman run -d --restart unless-stopped -p "127.0.0.1:3306:3306" --name=mysql8 -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:8.4
+			podman run -d --restart unless-stopped -p "127.0.0.1:3306:3306" --name=mysql8 -e MYSQL_ROOT_PASSWORD= -e MYSQL_ALLOW_EMPTY_PASSWORD=true docker.io/library/mysql:8.4
 			;;
 		Redis)
-			podman run -d --restart unless-stopped -p "127.0.0.1:6379:6379" --name=redis redis:7
+			podman run -d --restart unless-stopped -p "127.0.0.1:6379:6379" --name=redis docker.io/library/redis:7
 			;;
 		PostgreSQL)
-			podman run -d --restart unless-stopped -p "127.0.0.1:5432:5432" --name=postgres16 -e POSTGRES_HOST_AUTH_METHOD=trust postgres:16
+			podman run -d --restart unless-stopped -p "127.0.0.1:5432:5432" --name=postgres16 -e POSTGRES_HOST_AUTH_METHOD=trust docker.io/library/postgres:16
 			# Only attempt to set configuration if none is present
 			if [ ! -f "$HOME/.psqlrc" ]; then
 			  cp ~/.local/share/omakase-bluefin/configs/psqlrc ~/.psqlrc
