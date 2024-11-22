52cefd445bedda61fb262975bb55f50c027cea5f
Luke Hsiao <luke@hsiao.dev>
2024-11-22 01:48:43 -0700

    fix(install/terminal): s/cp/cd/

--- install/terminal/app-eisvogel.sh
+++ install/terminal/app-eisvogel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-cp /tmp
+cd /tmp
 curl -s https://api.github.com/repos/Wandmalfarbe/pandoc-latex-template/releases/latest | rg -N "browser_download_url" | rg -N --color never "Eisvogel.tar.gz" | sd '"' "'" | choose 1 | xargs -I % xh -F -o eisvogel.tar.gz GET %
 mkdir -p eisvogel
 mkdir -p ~/.pandoc/templates
