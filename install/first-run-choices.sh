109079683109c9b9a9b935dd3203c40820ca0651
Luke Hsiao <luke@hsiao.dev>
2024-11-22 02:14:02 -0700

    chore(install): reduce the default optional apps

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -1,5 +1,5 @@
 OPTIONAL_APPS=("Spotify" "Zoom" "Audacity" "Slack" "Zulip")
-DEFAULT_OPTIONAL_APPS='Spotify,Zoom,Audacity,Slack,Zulip'
+DEFAULT_OPTIONAL_APPS='Audacity,Slack'
 export OMAKASEBLUEFIN_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
 
 AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Rust" "Java")
