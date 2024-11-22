24e2c770dddd7b17027a80072fc19e8228079995
Luke Hsiao <luke@hsiao.dev>
2024-11-22 02:24:14 -0700

    fix(install/desktop): include wezterm config

--- install/desktop/app-wezterm.sh
+++ install/desktop/app-wezterm.sh
@@ -2,4 +2,12 @@ cd /tmp
 curl -LO https://github.com/wez/wezterm/releases/download/20240203-110809-5046fc22/WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage
 chmod +x WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage
 mv WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage ~/.local/bin/wezterm
+if [ ! -d "$HOME/.config/wezterm/wezterm.lua" ]; then
+  mkdir -p ~/.config/wezterm
+  cp ~/.local/share/omakase-bluefin/configs/wezterm.lua ~/.config/wezterm/wezterm.lue
+fi
+if [ ! -d "$HOME/.config/wezterm/colors" ]; then
+  mkdir -p ~/.config/wezterm/colors
+  cp ~/.local/share/omakase-bluefin/themes/selenized-dark/wezterm.toml ~/.config/wezterm/colors/selenized-dark.toml
+fi
 cd -
