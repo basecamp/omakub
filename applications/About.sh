2b41fae82c23f733762dc8369f78c709275b92a1
Luke Hsiao <luke@hsiao.dev>
2024-11-22 02:56:55 -0700

    fix(applications): fix about command in wezterm/fish

--- applications/About.sh
+++ applications/About.sh
@@ -3,7 +3,7 @@ cat <<EOF >~/.local/share/applications/About.desktop
 Version=1.0
 Name=About
 Comment=System information from Fastfetch
-Exec=wezterm start -- fastfetch; read -n 1 -s
+Exec=wezterm start -- bash -c 'fastfetch; read -n 1 -s'
 Terminal=false
 Type=Application
 Icon=/home/$USER/.local/share/omakase-bluefin/applications/icons/bluefin.webp
