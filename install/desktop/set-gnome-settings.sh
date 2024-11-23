879e8e7363ce9da40368f59dce2a9bb1d5202608
Luke Hsiao <luke@hsiao.dev>
2024-11-23 15:09:17 -0700

    fix(install/desktop): autohide the dock

--- install/desktop/set-gnome-settings.sh
+++ install/desktop/set-gnome-settings.sh
@@ -3,3 +3,9 @@ gsettings set org.gnome.desktop.interface monospace-font-name 'CaskaydiaMono Ner
 
 # Reveal week numbers in the Gnome calendar
 gsettings set org.gnome.desktop.calendar show-weekdate true
+
+# Autohide the dock
+gsettings set org.gnome.shell.extensions.dash-to-dock animation-time 0.0
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
+gsettings set org.gnome.shell.extensions.dash-to-dock intellihide true
+gsettings set org.gnome.shell.extensions.dash-to-dock intellihide-mode 'ALL_WINDOWS'
