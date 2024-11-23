a2aef85d01543152fb77faf2f689a37f6297b7d3
Luke Hsiao <luke@hsiao.dev>
2024-11-23 11:21:17 -0700

    chore: add TODO for thinkpad battery thresholds

--- install/desktop/set-gnome-extensions.sh
+++ install/desktop/set-gnome-extensions.sh
@@ -6,3 +6,7 @@ gum confirm "To install Gnome extensions, you need to accept some confirmations.
 
 # Install new extensions
 gext install paperwm@paperwm.github.com
+
+# TODO: make it optional to install Thinkpad Battery Threshold for those who want it.
+# Sometimes this is required on old Thinkpads to get the battery to charge.
+# gext install TODO
