937337feb8a5220d757d278e1b1b6a6e5311c2a7
Luke Hsiao <luke@hsiao.dev>
2024-11-23 14:52:43 -0700

    fix(install/terminal): overwrite fish config (with backup)

--- install/terminal/set-fish.sh
+++ install/terminal/set-fish.sh
@@ -1,8 +1,6 @@
-# Only attempt to set configuration if none is present
-if [ ! -d "$HOME/.config/fish" ]; then
-  mkdir -p ~/.config/fish
-  cp ~/.local/share/omakase-bluefin/configs/fish/config.fish ~/.config/fish/config.fish
-fi
+# Apparently, fish creates this be default, so we overwrite it.
+[ -f "~/.config/fish/config.fish" ] && mv ~/.config/fish/config.fish ~/.config/fish/config.fish.bak
+cp ~/.local/share/omakase-bluefin/configs/fish/config.fish ~/.config/fish/config.fish
 if [ ! -d "$HOME/.config/fish/functions/fish_command_not_found.fish" ]; then
   mkdir -p ~/.config/fish/functions
   cp ~/.local/share/omakase-bluefin/configs/fish/fish_command_not_found.fish ~/.config/fish/functions/fish_command_not_found.fish
