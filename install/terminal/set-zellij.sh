835f6b2813472f0ff5b40ec12346e17a77e99b5a
Luke Hsiao <luke@hsiao.dev>
2024-11-23 14:56:27 -0700

    fix(install/terminal): include zellij theme

--- install/terminal/set-zellij.sh
+++ install/terminal/set-zellij.sh
@@ -1,6 +1,6 @@
 # Only attempt to set configuration if none is present
 if [ ! -d "$HOME/.config/zellij/config.kdl" ]; then
-  mkdir -p ~/.config/zellij
+  mkdir -p ~/.config/zellij/themes
   cp ~/.local/share/omakase-bluefin/configs/zellij.kdl ~/.config/zellij/config.kdl
+  cp ~/.local/share/omakase-bluefin/selenized-dark/zellij.kdl ~/.config/zellij/themes/selenized-dark.kdl
 fi
-
