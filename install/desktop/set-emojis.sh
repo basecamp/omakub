0fcc6405f0e03b3d41437dd87e898225b5a93bd0
Luke Hsiao <luke@hsiao.dev>
2024-11-24 00:23:19 -0700

    chore(desktop): use ralt for compose key

--- install/desktop/set-emojis.sh
+++ install/desktop/set-emojis.sh
@@ -1,3 +1,3 @@
 cp ~/.local/share/omakase-bluefin/configs/xcompose ~/.XCompose
 ibus restart
-gsettings set org.gnome.desktop.input-sources xkb-options "['compose:caps']"
+gsettings set org.gnome.desktop.input-sources xkb-options "['compose:ralt']"
