44ea897d90af88869f7d052b13d031c0bf165d5a
Luke Hsiao <luke@hsiao.dev>
2024-11-30 09:11:54 -0700

    chore(install/desktop): show battery pecentage

--- install/desktop/set-gnome-settings.sh
+++ install/desktop/set-gnome-settings.sh
@@ -9,3 +9,6 @@ gsettings set org.gnome.shell.extensions.dash-to-dock animation-time 0.0
 gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
 gsettings set org.gnome.shell.extensions.dash-to-dock intellihide true
 gsettings set org.gnome.shell.extensions.dash-to-dock intellihide-mode 'ALL_WINDOWS'
+
+# Show Battery percentage in top bar
+gsettings set org.gnome.desktop.interface show-battery-percentage true
