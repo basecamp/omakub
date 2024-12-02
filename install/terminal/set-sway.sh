9c099f69df7b13eebed8e636e9244d4064e92b61
Luke Hsiao <luke@hsiao.dev>
2024-12-01 18:39:14 -0700

    chore: add swaylock config for lockscreen

--- install/terminal/set-sway.sh
+++ install/terminal/set-sway.sh
@@ -2,3 +2,7 @@ if [ ! -d "$HOME/.config/sway/config" ]; then
   mkdir -p ~/.config/sway
   cp ~/.local/share/omakase-blue/configs/sway/config ~/.config/sway/config
 fi
+if [ ! -d "$HOME/.config/swaylock/config" ]; then
+  mkdir -p ~/.config/swaylock
+  cp ~/.local/share/omakase-blue/configs/swaylock/config ~/.config/swaylock/config
+fi
