282f3d26069050d2e4ee246992d9a62860510b87
Luke Hsiao <luke@hsiao.dev>
2024-12-01 20:19:41 -0700

    fix(install/terminal): fix zellij theme path

--- install/terminal/set-zellij.sh
+++ install/terminal/set-zellij.sh
@@ -2,5 +2,5 @@
 if [ ! -d "$HOME/.config/zellij/config.kdl" ]; then
   mkdir -p ~/.config/zellij/themes
   cp ~/.local/share/omakase-blue/configs/zellij.kdl ~/.config/zellij/config.kdl
-  cp ~/.local/share/omakase-blue/selenized-dark/zellij.kdl ~/.config/zellij/themes/selenized-dark.kdl
+  cp ~/.local/share/omakase-blue/themes/selenized-dark/zellij.kdl ~/.config/zellij/themes/selenized-dark.kdl
 fi
