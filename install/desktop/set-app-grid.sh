1e5c8f80efff6fbb8237114950e658f4853ab5cb
Luke Hsiao <luke@hsiao.dev>
2024-12-02 10:11:16 -0700

    fix(install/desktop): only sudo rm if necessary

--- install/desktop/set-app-grid.sh
+++ install/desktop/set-app-grid.sh
@@ -1,2 +1,4 @@
 # App doesn't do anything when started from the app grid
-sudo rm -rf /var/lib/flatpak/exports/share/applications/org.flameshot.Flameshot.desktop
+if [ -f "/var/lib/flatpak/exports/share/applications/org.flameshot.Flameshot.desktop" ]; then
+  sudo rm -rf /var/lib/flatpak/exports/share/applications/org.flameshot.Flameshot.desktop
+fi
