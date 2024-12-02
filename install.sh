591b7e3d4856579f6ab0d4bd3681179735e6547b
Luke Hsiao <luke@hsiao.dev>
2024-12-02 09:32:54 -0700

    refactor(install): don't use systemd-inhibit
    It's easy enough to inhibit idle in sway already.

--- install.sh
+++ install.sh
@@ -4,28 +4,24 @@ set -e
 # Check the distribution name and version and abort if incompatible
 source ~/.local/share/omakase-blue/install/check-version.sh
 
-# TODO: Ensure computer doesn't go to sleep or lock while installing
-systemd-inhibit --what=idle:sleep --who="OmakaseBlue" --why="Long-running Script" --mode=block \
-bash -c '
-  # Desktop software and tweaks will only be installed if we're running Gnome
-  RUNNING_SWAY=$([[ "$XDG_CURRENT_DESKTOP" == *"sway"* ]] && echo true || echo false)
+# Desktop software and tweaks will only be installed if we're running Gnome
+RUNNING_SWAY=$([[ "$XDG_CURRENT_DESKTOP" == *"sway"* ]] && echo true || echo false)
 
-  if $RUNNING_SWAY; then
+if $RUNNING_SWAY; then
 
-    echo "Get ready to make a few choices..."
-    source ~/.local/share/omakase-blue/install/terminal/required/app-gum.sh >/dev/null
-    source ~/.local/share/omakase-blue/install/first-run-choices.sh
+  echo "Get ready to make a few choices..."
+  source ~/.local/share/omakase-blue/install/terminal/required/app-gum.sh >/dev/null
+  source ~/.local/share/omakase-blue/install/first-run-choices.sh
 
-    echo "Installing terminal and desktop tools..."
-  else
-    echo "Only installing terminal tools..."
-  fi
+  echo "Installing terminal and desktop tools..."
+else
+  echo "Only installing terminal tools..."
+fi
 
-  # Install terminal tools
-  source ~/.local/share/omakase-blue/install/terminal.sh
+# Install terminal tools
+source ~/.local/share/omakase-blue/install/terminal.sh
 
-  if $RUNNING_SWAY; then
-    # Install desktop tools and tweaks
-    source ~/.local/share/omakase-blue/install/desktop.sh
-  fi
-'
+if $RUNNING_SWAY; then
+  # Install desktop tools and tweaks
+  source ~/.local/share/omakase-blue/install/desktop.sh
+fi
