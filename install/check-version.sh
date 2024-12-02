d5b0256de44306153aebe4bf6b4d3a686d81dbba
Luke Hsiao <luke@hsiao.dev>
2024-12-01 19:11:02 -0700

    fix(install): wayblue sway is fedora

--- install/check-version.sh
+++ install/check-version.sh
@@ -9,10 +9,10 @@ fi
 . /etc/os-release
 
 # Check if running on Ubuntu 24.04 or higher
-if [ "$ID" != "blue" ] || [ $(echo "$VERSION_ID >= 41" | bc) != 1 ]; then
+if [ "$ID" != "fedora" ] || [ $(echo "$VERSION_ID >= 41" | bc) != 1 ]; then
     echo "$(tput setaf 1)Error: OS requirement not met"
     echo "You are currently running: $ID $VERSION_ID"
-    echo "OS required: Bluefin 41 or higher"
+    echo "OS required: Fedora 41 or higher"
     echo "Installation stopped."
     exit 1
 fi
