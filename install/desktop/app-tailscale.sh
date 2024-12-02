b5711e6bbeac83b022df9d9708c33828ef914bdb
Luke Hsiao <luke@hsiao.dev>
2024-12-01 18:40:42 -0700

    feat: add tailscale layering

---
+++ install/desktop/app-tailscale.sh
@@ -0,0 +1,4 @@
+# It is recommended to install VPNs via layering
+# Ref: https://discord.com/channels/1202304549294190602/1202306391499620402/1312951127418998806
+sudo curl -L https://pkgs.tailscale.com/stable/fedora/tailscale.repo -o /etc/yum.repos.d/tailscale.repo
+rpm-ostree install tailscale
