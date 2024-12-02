81653b34b5e876aa8d38be0247572655f4f33b63
Luke Hsiao <luke@hsiao.dev>
2024-12-01 20:20:51 -0700

    fix(install/terminal): eliminate user prompt on rm

--- install/terminal/app-git-sizer.sh
+++ install/terminal/app-git-sizer.sh
@@ -4,5 +4,5 @@ cd /tmp
 curl --proto '=https' --tlsv1.2 -sSLO https://github.com/github/git-sizer/releases/download/v1.5.0/git-sizer-1.5.0-linux-amd64.zip
 unzip -o git-sizer-1.5.0-linux-amd64.zip -d tmp-git-sizer
 install -m755 tmp-git-sizer/git-sizer $HOME/.local/bin/
-rm -r git-sizer-1.5.0-linux-amd64.zip tmp-git-sizer
+rm -rf git-sizer-1.5.0-linux-amd64.zip tmp-git-sizer
 cd -
