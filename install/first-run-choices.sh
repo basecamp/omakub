abab498944af87d3c4068c4787ad0f87e8337afa
Luke Hsiao <luke@hsiao.dev>
2024-12-06 20:18:40 -0700

    fix: show more options in without scrolling

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -1,6 +1,6 @@
 OPTIONAL_APPS=("Spotify" "Zoom" "Audacity" "Slack" "Zulip" "Inkscape" "DrawIO")
 DEFAULT_OPTIONAL_APPS='Audacity,Slack,Inkscape,DrawIO'
-export OMAKASEBLUE_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
+export OMAKASEBLUE_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 10 --header "Select optional apps" | tr ' ' '-')
 
 AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Rust" "Java")
 SELECTED_LANGUAGES="Rust","Python"
