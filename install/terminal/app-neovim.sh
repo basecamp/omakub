a99d3ec28cbd6c234f3508cc9a918f78ca299a22
han <icyfarhan@gmail.com>
2025-01-21 21:59:57 +0700

    Resolved the lazyvim checkhealth warnings for luarocks and tree-sitter

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -7,6 +7,9 @@ sudo cp -R nvim-linux-x86_64/share /usr/local/
 rm -rf nvim-linux-x86_64 nvim.tar.gz
 cd -
 
+# Install luarocks and tree-sitter-cli to resolve lazyvim :checkheatlh warnings
+sudo apt install -y luarocks tree-sitter-cli
+
 # Only attempt to set configuration if Neovim has never been run
 if [ ! -d "$HOME/.config/nvim" ]; then
 	# Use LazyVim
