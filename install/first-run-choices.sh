4e35b22350114b12a663c35fad3c57eef9058956
afonsomm <afonso@matosomagalhaes.com>
2025-01-21 01:29:44 +0000

    Add julia

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -2,7 +2,7 @@ OPTIONAL_APPS=("1password" "Spotify" "Zoom" "Dropbox")
 DEFAULT_OPTIONAL_APPS='1password,Spotify,Zoom'
 export OMAKUB_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
 
-AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Rust" "Java")
+AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Julia" "Elixir" "Rust" "Java")
 SELECTED_LANGUAGES="Ruby on Rails","Node.js"
 export OMAKUB_FIRST_RUN_LANGUAGES=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --selected "$SELECTED_LANGUAGES" --height 10 --header "Select programming languages")
 
