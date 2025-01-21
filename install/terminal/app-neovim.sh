b78a3c04586957ba622e94e4cdd3a6da764cdde8
han <icyfarhan@gmail.com>
2025-01-21 21:59:57 +0700

    Resolved the lazyvim checkhealth warnings for luarocks and tree-sitter

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -7,26 +7,29 @@ sudo cp -R nvim-linux64/share /usr/local/
 rm -rf nvim-linux64 nvim.tar.gz
 cd -
 
+# Install luarocks and tree-sitter-cli to resolve lazyvim :checkheatlh warnings
+sudo apt install -y luarocks tree-sitter-cli
+
 # Only attempt to set configuration if Neovim has never been run
 if [ ! -d "$HOME/.config/nvim" ]; then
-	# Use LazyVim
-	git clone https://github.com/LazyVim/starter ~/.config/nvim
-	# Remove the .git folder, so you can add it to your own repo later
-	rm -rf ~/.config/nvim/.git
+  # Use LazyVim
+  git clone https://github.com/LazyVim/starter ~/.config/nvim
+  # Remove the .git folder, so you can add it to your own repo later
+  rm -rf ~/.config/nvim/.git
 
-	# Make everything match the terminal transparency
-	mkdir -p ~/.config/nvim/plugin/after
-	cp ~/.local/share/omakub/configs/neovim/transparency.lua ~/.config/nvim/plugin/after/
+  # Make everything match the terminal transparency
+  mkdir -p ~/.config/nvim/plugin/after
+  cp ~/.local/share/omakub/configs/neovim/transparency.lua ~/.config/nvim/plugin/after/
 
-	# Default to Tokyo Night theme
-	cp ~/.local/share/omakub/themes/tokyo-night/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
+  # Default to Tokyo Night theme
+  cp ~/.local/share/omakub/themes/tokyo-night/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
 
-	# Enable default extras
-	cp ~/.local/share/omakub/configs/neovim/lazyvim.json ~/.config/nvim/lazyvim.json
+  # Enable default extras
+  cp ~/.local/share/omakub/configs/neovim/lazyvim.json ~/.config/nvim/lazyvim.json
 fi
 
 # Replace desktop launcher with one running inside Alacritty
 if [[ -d ~/.local/share/applications ]]; then
-	sudo rm -rf /usr/share/applications/nvim.desktop
-	source ~/.local/share/omakub/applications/Neovim.sh
+  sudo rm -rf /usr/share/applications/nvim.desktop
+  source ~/.local/share/omakub/applications/Neovim.sh
 fi
