223af974eb67f887a67d7bd2562dbaffb6858af6
afonsomm <afonso@matosomagalhaes.com>
2025-01-26 14:04:59 +0000

    set default theme when installing emacs

--- install/desktop/optional/app-doom-emacs.sh
+++ install/desktop/optional/app-doom-emacs.sh
@@ -1,3 +1,4 @@
 sudo apt install -y emacs
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom install
+source $OMAKUB_PATH/themes/tokyo-night/doom-emacs.sh
