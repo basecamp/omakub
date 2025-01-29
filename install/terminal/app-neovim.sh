4af8fdb886dd8d5f2bf8e06e514e6df788363076
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2025-01-29 20:26:55 +0700

    fix new neovim file naming

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -1,10 +1,10 @@
 cd /tmp
 wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz"
 tar -xf nvim.tar.gz
-sudo install nvim-linux64/bin/nvim /usr/local/bin/nvim
-sudo cp -R nvim-linux64/lib /usr/local/
-sudo cp -R nvim-linux64/share /usr/local/
-rm -rf nvim-linux64 nvim.tar.gz
+sudo install nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+sudo cp -R nvim-linux-x86_64/lib /usr/local/
+sudo cp -R nvim-linux-x86_64/share /usr/local/
+rm -rf nvim-linux-x86_64 nvim.tar.gz
 cd -
 
 # Only attempt to set configuration if Neovim has never been run
