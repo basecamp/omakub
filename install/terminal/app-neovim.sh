06b5874a1153b7421abc87c602887fbcba7cb616
Ahmed Shahwan <6496157+shahwan42@users.noreply.github.com>
2025-01-30 03:07:41 +0200

    Revert: Only fix the URL and extraction directory name

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -1,7 +1,10 @@
 cd /tmp
-wget -O nvim https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage
-chmod +x nvim
-sudo mv nvim /usr/local/bin/
+wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz"
+tar -xf nvim.tar.gz
+sudo install nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+sudo cp -R nvim-linux-x86_64/lib /usr/local/
+sudo cp -R nvim-linux-x86_64/share /usr/local/
+rm -rf nvim-linux-x86_64 nvim.tar.gz
 cd -
 
 # Only attempt to set configuration if Neovim has never been run
