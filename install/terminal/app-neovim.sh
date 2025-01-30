c54530a09b70ce65d6cb5cc354f0e5e2f24dd608
Ahmed Shahwan <6496157+shahwan42@users.noreply.github.com>
2025-01-30 02:54:20 +0200

    fix neovim installation for recent versions

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -1,10 +1,7 @@
 cd /tmp
-wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
-tar -xf nvim.tar.gz
-sudo install nvim-linux64/bin/nvim /usr/local/bin/nvim
-sudo cp -R nvim-linux64/lib /usr/local/
-sudo cp -R nvim-linux64/share /usr/local/
-rm -rf nvim-linux64 nvim.tar.gz
+wget -O nvim https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage
+chmod +x nvim
+sudo mv nvim /usr/local/bin/
 cd -
 
 # Only attempt to set configuration if Neovim has never been run
