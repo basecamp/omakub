6609d17164db9fc49d54d9d4fb701213d2c19f69
Eric Gusmao <ericdegusmaopino@gmail.com>
2025-02-03 18:39:38 -0300

    Set default PHP version to 8.4 on installation

--- install/terminal/select-dev-language.sh
+++ install/terminal/select-dev-language.sh
@@ -21,7 +21,7 @@ if [[ -n "$languages" ]]; then
 			;;
 		PHP)
 			sudo add-apt-repository -y ppa:ondrej/php
-			sudo apt -y install php8.3 php8.3-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
+			sudo apt -y install php8.4 php8.4-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
 			php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 			php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
 			rm composer-setup.php
