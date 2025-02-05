84f9e4a3a48c060a1eeeabf0d01327361f8ca661
Tony Messias <tonysm@hey.com>
2025-02-05 11:35:13 -0300

    Rename variables

--- install/terminal/select-dev-language.sh
+++ install/terminal/select-dev-language.sh
@@ -1,61 +1,66 @@
 # Install default programming languages
 if [[ -v OMAKUB_FIRST_RUN_LANGUAGES ]]; then
-	languages=$OMAKUB_FIRST_RUN_LANGUAGES
+    languages=$OMAKUB_FIRST_RUN_LANGUAGES
 else
-	AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Rust" "Java")
-	languages=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --height 10 --header "Select programming languages")
+    AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Rust" "Java")
+    languages=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --height 10 --header "Select programming languages")
 fi
 
 enable_lazyvim_extras() {
-	local temp_file=$(mktemp /tmp/omakub.XXXXX)
-	local extras=("$@")
-	local jq_extras=$(printf '"%s",' "${extras[@]}")
-	jq_extras="[${jq_extras%,}]"
+    local config_file="$HOME/.config/nvim/lazyvim.json"
+    local extras=("$@")
 
-	jq --argjson extras "$jq_extras" '.extras |= (. + $extras | unique)' ~/.config/nvim/lazyvim.json >"$temp_file" &&
-	mv "$temp_file" ~/.config/nvim/lazyvim.json
+    local tmp_file
+    tmp_file=$(mktemp /tmp/omakub.XXXXX)
+
+    local extras_json
+    extras_json=$(printf '"%s",' "${extras[@]}")
+    extras_json="[${extras_json%,}]"
+
+    jq --argjson extras "$extras_json" '.extras |= (. + $extras | unique)' "$config_file" >"$tmp_file"
+    mv "$tmp_file" "$config_file"
 }
 
 if [[ -n "$languages" ]]; then
-	for language in $languages; do
-		case $language in
-		Ruby)
-			mise use --global ruby@3.3
-			mise x ruby -- gem install rails --no-document
-			;;
-		Node.js)
-			mise use --global node@lts
+    for language in $languages; do
+        case $language in
+        Ruby)
+            mise use --global ruby@3.3
+            mise x ruby -- gem install rails --no-document
+            ;;
+        Node.js)
+            mise use --global node@lts
 
-			enable_lazyvim_extras "lazyvim.plugins.extras.lang.typescript"
-			;;
-		Go)
-			mise use --global go@latest
+            enable_lazyvim_extras "lazyvim.plugins.extras.lang.typescript"
+            ;;
+        Go)
+            mise use --global go@latest
 
-			enable_lazyvim_extras "lazyvim.plugins.extras.lang.go"
-			;;
-		PHP)
-			sudo add-apt-repository -y ppa:ondrej/php
-			sudo apt -y install php8.3 php8.3-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
-			php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-			php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
-			rm composer-setup.php
+            enable_lazyvim_extras "lazyvim.plugins.extras.lang.go"
+            ;;
+        PHP)
+            sudo add-apt-repository -y ppa:ondrej/php
+            sudo apt -y install php8.3 php8.3-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
+            php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+            php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
+            rm composer-setup.php
 
-			enable_lazyvim_extras "lazyvim.plugins.extras.lang.php"
-			;;
-		Python)
-			mise use --global python@latest
-			;;
-		Elixir)
-			mise use --global erlang@latest
-			mise use --global elixir@latest
-			mise x elixir -- mix local.hex --force
-			;;
-		Rust)
-			bash -c "$(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs)" -- -y
-			;;
-		Java)
-			mise use --global java@latest
-			;;
-		esac
-	done
+            enable_lazyvim_extras "lazyvim.plugins.extras.lang.php"
+            ;;
+        Python)
+            mise use --global python@latest
+            ;;
+        Elixir)
+            mise use --global erlang@latest
+            mise use --global elixir@latest
+            mise x elixir -- mix local.hex --force
+            ;;
+        Rust)
+            bash -c "$(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs)" -- -y
+            ;;
+        Java)
+            mise use --global java@latest
+            ;;
+        esac
+    done
 fi
