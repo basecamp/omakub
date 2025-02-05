4e19fb9e6871e84fddc2ebc0d870572488b05ac1
David Heinemeier Hansson <david@37signals.com>
2025-02-05 19:46:57 +0100

    Reboot required for udev rules to be picked up

--- install/desktop/optional/app-asdcontrol.sh
+++ install/desktop/optional/app-asdcontrol.sh
@@ -10,3 +10,6 @@ rm -rf /tmp/asdcontrol
 echo 'KERNEL=="hiddev*", ATTRS{idVendor}=="05ac", ATTRS{idProduct}=="9243", GROUP="users", OWNER="root", MODE="0660"' | sudo tee /etc/udev/rules.d/50-apple-xdr.rules >/dev/null
 echo 'KERNEL=="hiddev*", ATTRS{idVendor}=="05ac", ATTRS{idProduct}=="1114", GROUP="users", OWNER="root", MODE="0660"' | sudo tee /etc/udev/rules.d/50-apple-studio.rules >/dev/null
 sudo udevadm control --reload-rules
+
+# Reboot to pickup changes
+gum confirm "Ready to reboot for brightness controls to be available?" && sudo reboot
