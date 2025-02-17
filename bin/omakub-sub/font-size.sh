c6b493de180089bcd54f9f98c037fcc16ef86e3b
ofrades <ofrades@pm.me>
2025-02-17 22:45:25 +0000

    fix font-size script

--- bin/omakub-sub/font-size.sh
+++ bin/omakub-sub/font-size.sh
@@ -1,7 +1,7 @@
 choice=$(gum choose {7..14} "<< Back" --height 11 --header "Choose your terminal font size")
 
 if [[ $choice =~ ^[0-9]+$ ]]; then
-  sed -i "s/^size = .*$/size = $choice/g" ~/.config/ghostty/font-size
+  sed -i "s/^font-size = .*$/font-size = $choice/g" ~/.config/ghostty/font-size
   source $OMAKUB_PATH/bin/omakub-sub/font-size.sh
 else
   source $OMAKUB_PATH/bin/omakub-sub/font.sh
