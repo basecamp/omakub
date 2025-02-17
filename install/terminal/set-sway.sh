365bb896c0b923d452571b466fd77ae1e995710e
Luke Hsiao <luke@hsiao.dev>
2025-02-17 15:18:44 -0700

    chore(configs): show date and time in waybar

--- install/terminal/set-sway.sh
+++ install/terminal/set-sway.sh
@@ -6,3 +6,7 @@ if [ ! -d "$HOME/.config/swaylock/config" ]; then
   mkdir -p ~/.config/swaylock
   cp ~/.local/share/omakase-blue/configs/swaylock/config ~/.config/swaylock/config
 fi
+if [ ! -d "$HOME/.config/waybar/config.jsonc" ]; then
+  mkdir -p ~/.config/waybar
+  cp ~/.local/share/omakase-blue/configs/waybar/config.jsonc ~/.config/waybar/config.jsonc
+fi
