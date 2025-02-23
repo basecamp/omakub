81c6b58d94d1bc09a2a988a4ee1d92e472b4d93a
billybonks <sebastienstettler@gmail.com>
2025-02-23 20:08:15 +0800

    refactor: replace apt-get with just apt
    i was exploring why we are running update many places in the codebase
    and noticed we used apt-get in the start but it doesnt seem required, so
    proposing to stream line the commands with just one

--- boot.sh
+++ boot.sh
@@ -12,8 +12,8 @@ echo -e "$ascii_art"
 echo "=> Omakub is for fresh Ubuntu 24.04+ installations only!"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
-sudo apt-get update >/dev/null
-sudo apt-get install -y git >/dev/null
+sudo apt update >/dev/null
+sudo apt install -y git >/dev/null
 
 echo "Cloning Omakub..."
 rm -rf ~/.local/share/omakub
