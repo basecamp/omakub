f43371f80185e0ffa577aefc61a02cc4e90ef3c6
David Heinemeier Hansson <david@37signals.com>
2025-03-04 10:47:25 +0100

    Simplify the installer (closes #316)
    Co-author: asmrtfm

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Desktop software and tweaks will only be installed if we're running Gnome
-RUNNING_GNOME=$([[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]] && echo true || echo false)
-
 # Check the distribution name and version and abort if incompatible
 source ~/.local/share/omakub/install/check-version.sh
 
-if $RUNNING_GNOME; then
+# Desktop software and tweaks will only be installed if we're running Gnome
+if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
   # Ensure computer doesn't go to sleep or lock while installing
   gsettings set org.gnome.desktop.screensaver lock-enabled false
   gsettings set org.gnome.desktop.session idle-delay 0
@@ -17,18 +15,17 @@ if $RUNNING_GNOME; then
   source ~/.local/share/omakub/install/first-run-choices.sh
 
   echo "Installing terminal and desktop tools..."
-else
-  echo "Only installing terminal tools..."
-fi
 
-# Install terminal tools
-source ~/.local/share/omakub/install/terminal.sh
+  # Install terminal tools
+  source ~/.local/share/omakub/install/terminal.sh
 
-if $RUNNING_GNOME; then
   # Install desktop tools and tweaks
   source ~/.local/share/omakub/install/desktop.sh
 
   # Revert to normal idle and lock settings
   gsettings set org.gnome.desktop.screensaver lock-enabled true
   gsettings set org.gnome.desktop.session idle-delay 300
+else
+  echo "Only installing terminal tools..."
+  source ~/.local/share/omakub/install/terminal.sh
 fi
