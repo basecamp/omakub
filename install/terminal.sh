14142e86ff7b7d7baca95c848fc3c6505584ca57
han <icyfarhan@gmail.com>
2025-03-25 22:10:09 +0700

    Initial commit, now the check-version.sh and terminal.sh will determine when to use apt and dnf using environment variable

--- install/terminal.sh
+++ install/terminal.sh
@@ -1,7 +1,16 @@
-# Needed for all installers
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y curl git unzip
+if [ "$OMAKUB_OS" = "ubuntu" ]; then
+  # Needed for all installers
+  sudo apt update -y
+  sudo apt upgrade -y
+  sudo apt install -y curl git unzip
+elif [ "$OMAKUB_OS" = "fedora" ]; then
+  # Needed for all installers
+  sudo dnf update -y
+  sudo dnf upgrade -y
+  sudo dnf install -y curl git unzip
+fi
+
+
 
 # Run terminal installers
 for installer in ~/.local/share/omakub/install/terminal/*.sh; do source $installer; done
