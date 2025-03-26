b565064af7c94c85be21b09768e0af5f4d9eeba5
han <icyfarhan@gmail.com>
2025-03-26 11:52:31 +0700

    Completed all the required .sh to be compatible with fedora

--- install/terminal/select-dev-language.sh
+++ install/terminal/select-dev-language.sh
@@ -20,11 +20,20 @@ if [[ -n "$languages" ]]; then
       mise use --global go@latest
       ;;
     PHP)
-      sudo add-apt-repository -y ppa:ondrej/php
-      sudo apt -y install php8.4 php8.4-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
-      php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-      php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
-      rm composer-setup.php
+      if [ "$OMAKUB_OS" = "ubuntu" ]; then
+        sudo add-apt-repository -y ppa:ondrej/php
+        sudo apt -y install php8.4 php8.4-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
+        php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+        php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
+        rm composer-setup.php
+      elif [ "$OMAKUB_OS" = "fedora" ]; then
+        sudo dnf install -y https://rpms.remirepo.net/fedora/remi-release-$(rpm -E %fedora).rpm
+        sudo dnf module enable -y php:remi-8.4
+        sudo dnf install -y php php-{curl,apcu,intl,mbstring,opcache,pgsql,mysqlnd,sqlite3,redis,xml,zip}
+        php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+        php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
+        rm composer-setup.php
+      fi
       ;;
     Python)
       mise use --global python@latest
