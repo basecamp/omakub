2537e3a5f6e2cc027a95a19e456363f6e0631437
han <icyfarhan@gmail.com>
2025-03-26 14:27:37 +0700

    Use the correct about app icon for both fedora and ubuntu, and make the bash-completation and fzf folder manually in fedora

--- applications/About.sh
+++ applications/About.sh
@@ -1,4 +1,12 @@
-cat <<EOF >~/.local/share/applications/About.desktop
+# Set icon path based on OS
+if [ "$OMAKUB_OS" = "ubuntu" ]; then
+  ICON_FILE="Ubuntu.png"
+elif [ "$OMAKUB_OS" = "fedora" ]; then
+  ICON_FILE="Fedora.png"
+fi
+
+# Create the desktop file with dynamic icon
+cat <<EOF > ~/.local/share/applications/About.desktop
 [Desktop Entry]
 Version=1.0
 Name=About
@@ -6,7 +14,7 @@ Comment=System information from Fastfetch
 Exec=alacritty --config-file /home/$USER/.local/share/omakub/defaults/alacritty/pane.toml --class=About --title=About -e bash -c 'fastfetch; read -n 1 -s'
 Terminal=false
 Type=Application
-Icon=/home/$USER/.local/share/omakub/applications/icons/Ubuntu.png
+Icon=/home/$USER/.local/share/omakub/applications/icons/$ICON_FILE
 Categories=GTK;
 StartupNotify=false
 EOF
