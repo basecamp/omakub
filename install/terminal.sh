da6017ef71a2c11aac09b3f8ad714a7095675868
Renato Viço <renato@s2n.com.br>
2025-04-08 20:11:27 +0200

    Terminal

--- install/terminal.sh
+++ install/terminal.sh
@@ -1,7 +1,7 @@
 # Needed for all installers
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y curl git unzip
+sudo dnf update -y
+sudo dnf upgrade -y
+sudo dnf install -y curl git unzip
 
 # Run terminal installers
 for installer in ~/.local/share/omakub/install/terminal/*.sh; do source $installer; done
