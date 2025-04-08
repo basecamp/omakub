17f247b847fb07f885d90cfccf3f253bc50b158a
Renato Viço <renato@s2n.com.br>
2025-04-08 19:34:04 +0200

    Neovim

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -8,7 +8,7 @@ rm -rf nvim-linux-x86_64 nvim.tar.gz
 cd -
 
 # Install luarocks and tree-sitter-cli to resolve lazyvim :checkhealth warnings
-sudo apt install -y luarocks tree-sitter-cli
+sudo dnf install -y luarocks tree-sitter-cli
 
 # Only attempt to set configuration if Neovim has never been run
 if [ ! -d "$HOME/.config/nvim" ]; then
