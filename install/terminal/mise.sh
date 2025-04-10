80ac91210f30b355ea328bc74894b95d7d561446
Renato Viço <renato@s2n.com.br>
2025-04-10 14:20:51 +0200

    Port Mise

--- install/terminal/mise.sh
+++ install/terminal/mise.sh
@@ -1,7 +1,4 @@
 # Install mise for managing multiple versions of languages. See https://mise.jdx.dev/
-sudo dnf update -y && sudo dnf install -y gpg wget curl
-sudo install -dm 755 /etc/dnf/keyrings
-wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/dnf/keyrings/mise-archive-keyring.gpg 1>/dev/null
-echo "deb [signed-by=/etc/dnf/keyrings/mise-archive-keyring.gpg arch=$(dpkg --print-architecture)] https://mise.jdx.dev/deb stable main" | sudo tee /etc/dnf/sources.list.d/mise.list
-sudo dnf update
+sudo dnf install -y dnf-plugins-core
+sudo dnf config-manager addrepo --from-repofile=https://mise.jdx.dev/rpm/mise.repo
 sudo dnf install -y mise
