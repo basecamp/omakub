7df8677071f434d1c7d2cd7ab3abe6b26b3941c2
David Heinemeier Hansson <david@hey.com>
2025-04-19 18:29:13 +0200

    Switch RetroArch to flatpak for Ubuntu 25.04 compatibility
    Closes #438

--- install/desktop/optional/app-retroarch.sh
+++ install/desktop/optional/app-retroarch.sh
@@ -1,14 +1,1 @@
-sudo add-apt-repository -y ppa:libretro/stable
-sudo apt update -y
-sudo apt install -y retroarch retroarch-assets
-
-cat <<EOF >~/.local/share/applications/Retroarch.desktop
-[Desktop Entry]
-Name=RetroArch
-Exec=/usr/bin/retroarch
-Type=Application
-Icon=/home/$USER/.local/share/omakub/applications/icons/Retroarch.png
-Comment=Play retro games
-Categories=Game;Emulator;
-Terminal=false
-EOF
+flatpak install -y flathub org.libretro.RetroArch
--- uninstall/app-retroarch.sh
+++ uninstall/app-retroarch.sh
@@ -1,5 +1,1 @@
-sudo add-apt-repository -y --remove ppa:libretro/stable
-sudo apt update -y
-sudo apt remove -y retroarch retroarch-assets
-sudo apt autoremove -y
-rm ~/.local/share/applications/Retroarch.desktop
+flatpak uninstall -y flathub org.libretro.RetroArch
