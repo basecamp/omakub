558408e096022ceae68c038a666e0ab94307872b
David Heinemeier Hansson <david@hey.com>
2025-04-19 10:37:30 +0200

    Ensure modprobes are gone too

--- install/desktop/optional/app-virtualbox.sh
+++ install/desktop/optional/app-virtualbox.sh
@@ -5,6 +5,10 @@
 # First remove any conflicting virtualization options
 sudo apt remove --purge -y qemu-kvm libvirt-daemon virt-manager
 
+# Ensure kvm modprobes aren't still loaded
+sudo modprobe -r kvm_amd
+sudo modprobe -r kvm
+
 sudo apt update
 sudo apt install -y gcc-14 virtualbox virtualbox-ext-pack
 sudo usermod -aG vboxusers ${USER}
