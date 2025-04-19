ab821e382d17f343aa1033bbe10eaf13764ce9be
David Heinemeier Hansson <david@hey.com>
2025-04-19 11:20:42 +0200

    Lets try without this for a second on a clean install

--- install/desktop/optional/app-virtualbox.sh
+++ install/desktop/optional/app-virtualbox.sh
@@ -2,13 +2,6 @@
 # See https://ubuntu.com/tutorials/how-to-run-ubuntu-desktop-on-a-virtual-machine-using-virtualbox#1-overview
 # for a guide on how to run Ubuntu inside it.
 
-# First remove any conflicting virtualization options
-sudo apt remove --purge -y qemu-kvm libvirt-daemon virt-manager
-
-# Ensure kvm modprobes aren't still loaded
-sudo modprobe -r kvm_amd
-sudo modprobe -r kvm
-
 sudo apt update
-sudo apt install -y gcc-14 virtualbox virtualbox-ext-pack
+sudo apt install -y virtualbox virtualbox-ext-pack
 sudo usermod -aG vboxusers ${USER}
