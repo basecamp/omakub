217ee95d2a79ddce50178e47b41276a654a18eaf
David Heinemeier Hansson <david@hey.com>
2025-04-19 17:04:35 +0200

    Give people a chance to retry a failing installation

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Give people a chance to retry running the installation
+trap 'echo "Omakub installation failed! You can retry by running: source ~/.local/share/omakub/install.sh"' ERR
+
 # Check the distribution name and version and abort if incompatible
 source ~/.local/share/omakub/install/check-version.sh
 
