013727615567ec7edecf25df3d83c8aed7bce57c
David Heinemeier Hansson <david@hey.com>
2025-04-19 11:57:59 +0200

    Proper signing

--- install/desktop/ulauncher.sh
+++ install/desktop/ulauncher.sh
@@ -1,6 +1,6 @@
 gpg --keyserver keyserver.ubuntu.com --recv 0xfaf1020699503176
 gpg --export 0xfaf1020699503176 | sudo tee /usr/share/keyrings/ulauncher-archive-keyring.gpg >/dev/null
-echo "deb http://ppa.launchpad.net/agornostal/ulauncher/ubuntu noble main" | sudo tee /etc/apt/sources.list.d/ulauncher-noble.list
+echo "deb [signed-by=/usr/share/keyrings/ulauncher-archive-keyring.gpg] http://ppa.launchpad.net/agornostal/ulauncher/ubuntu noble main" | sudo tee /etc/apt/sources.list.d/ulauncher-noble.list
 
 sudo apt update -y
 sudo apt install -y ulauncher
