86e0f0fe2b6168d62590b0f07e7e9b02b15c4440
Luca Pattocchio <pattocchioluca@gmail.com>
2025-05-05 21:14:45 +0200

    fix: update nvim theme only if it s installed

--- bin/omakub-sub/theme.sh
+++ bin/omakub-sub/theme.sh
@@ -5,7 +5,9 @@ if [ -n "$THEME" ] && [ "$THEME" != "<<-back" ]; then
   cp $OMAKUB_PATH/themes/$THEME/alacritty.toml ~/.config/alacritty/theme.toml
   cp $OMAKUB_PATH/themes/$THEME/zellij.kdl ~/.config/zellij/themes/$THEME.kdl
   sed -i "s/theme \".*\"/theme \"$THEME\"/g" ~/.config/zellij/config.kdl
-  cp $OMAKUB_PATH/themes/$THEME/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
+  if [ -d "$HOME/.config/nvim" ]; then
+    cp $OMAKUB_PATH/themes/$THEME/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
+  fi
 
   source $OMAKUB_PATH/themes/$THEME/gnome.sh
   source $OMAKUB_PATH/themes/$THEME/tophat.sh
