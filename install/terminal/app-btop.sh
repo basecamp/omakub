4de4a1659f81a47aaf2498859ca1dc0d30be16d5
David Heinemeier Hansson <david@hey.com>
2025-05-18 21:19:14 +0200

    Ensure default theme is present

--- install/terminal/app-btop.sh
+++ install/terminal/app-btop.sh
@@ -4,4 +4,5 @@ sudo apt install -y btop
 # Use Omakub btop config
 mkdir -p ~/.config/btop
 cp ~/.local/share/omakub/configs/btop.conf ~/.config/btop/btop.conf
+cp ~/.local/share/omakub/themes/tokyo-night/btop.theme ~/.config/btop/themes/tokyo-night.theme
 
