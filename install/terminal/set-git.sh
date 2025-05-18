44b8f9d0677a317724cb9d1a309928cdf91c8f92
David Heinemeier Hansson <david@hey.com>
2025-05-18 20:59:41 +0200

    Ensure we only set if there is some data in these variables

--- install/terminal/set-git.sh
+++ install/terminal/set-git.sh
@@ -6,5 +6,10 @@ git config --global alias.st status
 git config --global pull.rebase true
 
 # Set identification from install inputs
-git config --global user.name "$OMAKUB_USER_NAME"
-git config --global user.email "$OMAKUB_USER_EMAIL"
+if [[ -n "${OMAKUB_USER_NAME//[[:space:]]/}" ]]; then
+  git config --global user.name "$OMAKUB_USER_NAME"
+fi
+
+if [[ -n "${OMAKUB_USER_EMAIL//[[:space:]]/}" ]]; then
+  git config --global user.email "$OMAKUB_USER_EMAIL"
+fi
