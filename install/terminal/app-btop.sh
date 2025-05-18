8e726305879b241f7d4d86ee4f48cb8225449984
nexckycort <nexckycort@gmail.com>
2025-05-18 18:36:56 -0500

    Fix directory creation for btop themes

--- install/terminal/app-btop.sh
+++ install/terminal/app-btop.sh
@@ -2,7 +2,7 @@
 sudo apt install -y btop
 
 # Use Omakub btop config
-mkdir -p ~/.config/btop
+mkdir -p ~/.config/btop/themes
 cp ~/.local/share/omakub/configs/btop.conf ~/.config/btop/btop.conf
 cp ~/.local/share/omakub/themes/tokyo-night/btop.theme ~/.config/btop/themes/tokyo-night.theme
 
