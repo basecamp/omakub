3cff8cc594ee4688278bbefae8149e625cde4e76
David Heinemeier Hansson <david@hey.com>
2025-05-18 21:04:06 +0200

    Always set config

--- install/terminal/app-btop.sh
+++ install/terminal/app-btop.sh
@@ -1,9 +1,7 @@
 # This script installs btop, a resource monitor that shows usage and stats for processor, memory, disks, network and processes.
 sudo apt install -y btop
 
-# Only attempt to set configuration if btop is not already set
-if [ ! -f "$HOME/.config/btop/btop.conf" ]; then
-  # Use Omakub btop config
-  mkdir -p ~/.config/btop
-  cp ~/.local/share/omakub/configs/btop.conf ~/.config/btop/btop.conf
-fi
+# Use Omakub btop config
+mkdir -p ~/.config/btop
+cp ~/.local/share/omakub/configs/btop.conf ~/.config/btop/btop.conf
+
