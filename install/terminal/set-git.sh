37829cd11fbd7be47fcd0637255524d99e7484e4
David Heinemeier Hansson <david@hey.com>
2025-05-18 17:13:32 +0200

    Need escaping

--- install/terminal/set-git.sh
+++ install/terminal/set-git.sh
@@ -6,5 +6,5 @@ git config --global alias.st status
 git config --global pull.rebase true
 
 # Set identification from install inputs
-git config --global user.name $OMAKUB_USER_NAME
-git config --global user.email $OMAKUB_USER_EMAIL
+git config --global user.name "$OMAKUB_USER_NAME"
+git config --global user.email "$OMAKUB_USER_EMAIL"
