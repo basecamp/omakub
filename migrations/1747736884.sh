052099982743421a0cd909bb65a8af55aa29c74c
David Heinemeier Hansson <david@hey.com>
2025-05-20 12:35:04 +0200

    Add migration to ensure we have migrated all alacritty configs

---
+++ migrations/1747736884.sh
@@ -0,0 +1,4 @@
+# Ensure all alacritty configs have been migrated
+alacritty migrate
+alacritty migrate --config-file ~/.config/alacritty/btop.toml
+alacritty migrate --config-file ~/.config/alacritty/pane.toml
