aae694516ba60185486912f5c58bffe2fbb544b0
Joel AZEMAR <joel.azemar@gmail.com>
2025-05-31 11:32:59 +0200

    Add Missing Helper

--- install/desktop/optional/app-firefox-nightly.sh
+++ install/desktop/optional/app-firefox-nightly.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Install Firefox Nightly
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-OMAKUB_PATH="${OMAKUB_PATH:-$SCRIPT_DIR/../../..}"
-source "$OMAKUB_PATH/src/utils/firefox-helpers.sh"
+source "$OMAKUB_PATH/utils/firefox-helpers.sh"
+
 NIGHTLY_URL="https://download.mozilla.org/?product=firefox-nightly-latest-ssl&os=linux64&lang=en-US"
 download_and_extract "Firefox Nightly" "$NIGHTLY_URL" "firefox-nightly" "firefox-nightly" "Firefox Nightly" "Web Browser (Nightly)"
