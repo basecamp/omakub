761ae93761fa97cddd72decdd7415b5c7a85c152
Huska, Matthew <HuskaM@rki.de>
2025-06-17 11:12:55 +0200

    Add -y to element-desktop install

--- install/desktop/app-element.sh
+++ install/desktop/app-element.sh
@@ -2,4 +2,4 @@ sudo apt install -y wget apt-transport-https
 sudo wget -O /usr/share/keyrings/element-io-archive-keyring.gpg https://packages.element.io/debian/element-io-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/element-io-archive-keyring.gpg] https://packages.element.io/debian/ default main" | sudo tee /etc/apt/sources.list.d/element-io.list
 sudo apt update
-sudo apt install element-desktop
+sudo apt install -y element-desktop
