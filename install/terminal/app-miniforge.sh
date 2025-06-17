9d3e56715f191f4bb77611ebbcaa86b9500c01af
Huska, Matthew <HuskaM@rki.de>
2025-06-17 11:24:48 +0200

    Force install miniforge

--- install/terminal/app-miniforge.sh
+++ install/terminal/app-miniforge.sh
@@ -1,3 +1,3 @@
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-bash Miniforge3-$(uname)-$(uname -m).sh -b
+bash Miniforge3-$(uname)-$(uname -m).sh -b -f
 
