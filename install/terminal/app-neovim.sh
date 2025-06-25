03f99a4bf18e684cf40508d7e32ae136b0499e05
Huska, Matthew <HuskaM@rki.de>
2025-06-25 15:18:47 +0200

    Stop setting app themes to Tokyo Night

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -22,7 +22,7 @@ if [ ! -d "$HOME/.config/nvim" ]; then
   cp ~/.local/share/omakub/configs/neovim/transparency.lua ~/.config/nvim/plugin/after/
 
   # Default to Tokyo Night theme
-  cp ~/.local/share/omakub/themes/tokyo-night/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
+  #cp ~/.local/share/omakub/themes/tokyo-night/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
 
   # Turn off animated scrolling
   cp ~/.local/share/omakub/configs/neovim/snacks-animated-scrolling-off.lua ~/.config/nvim/lua/plugins/
