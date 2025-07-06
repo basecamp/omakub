a5b0a6a349e3791b71a34477ea61d37d15f61b34
David Heinemeier Hansson <david@hey.com>
2025-07-06 10:23:17 -0700

    Fix install line

--- install/desktop/app-typora.sh
+++ install/desktop/app-typora.sh
@@ -10,7 +10,7 @@
 # Install with db
 cd /tmp
 wget -O typora.deb "https://downloads.typora.io/linux/typora_1.10.8_amd64.deb"
-sudo apt install -y typora.deb
+sudo apt install -y /tmp/typora.deb
 rm typora.deb
 cd -
 
