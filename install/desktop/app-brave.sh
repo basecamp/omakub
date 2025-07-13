d59e79eeb81091dd762786b8b1bbf3bde14bf0d0
Aaron Stannard <aaron@petabridge.com>
2025-07-12 21:59:34 -0500

    Make Brave the default browser (#3)

--- install/desktop/app-brave.sh
+++ install/desktop/app-brave.sh
@@ -2,3 +2,5 @@ sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://b
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update -y
 sudo apt install -y brave-browser
+xdg-settings set default-web-browser brave-browser.desktop
+sudo update-alternatives --install /usr/bin/x-www-browser x-www-browser /usr/bin/brave-browser 200
