75275402073b7d2507a7c6e5823e584d58b15f0c
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-13 10:15:31 +0200

    upd: add discord app

---
+++ install/desktop/optional/app-discord.sh
@@ -0,0 +1,6 @@
+# A Communication platform for voice, video, and text messaging https://discord.com/
+cd /tmp
+wget https://discord.com/api/download?platform=linux -O discord.deb
+sudo apt install ./discord.deb -y
+rm discord.deb
+cd -
---
+++ uninstall/app-discord.sh
@@ -0,0 +1,1 @@
+sudo apt remove --purge -y discord
