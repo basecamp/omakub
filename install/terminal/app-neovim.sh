3af8bff689de88fc8330c80c3a16ea84fbb0b8e7
David Heinemeier Hansson <david@hey.com>
2025-07-29 08:47:34 +0200

    Use neo-tree by default on new installs

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -31,6 +31,9 @@ if [ ! -d "$HOME/.config/nvim" ]; then
 
   # Turn off relative line numbers
   echo "vim.opt.relativenumber = false" >>~/.config/nvim/lua/config/options.lua
+
+  # Ensure editor.neo-tree is used by default
+  cp ~/.local/share/omakub/configs/neovim/lazyvim.json ~/.config/nvim/
 fi
 
 # Replace desktop launcher with one running inside Alacritty
