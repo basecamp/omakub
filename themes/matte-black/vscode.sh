0a96ffed094a286242fb04151253ccd2daad95f9
David Heinemeier Hansson <david@hey.com>
2025-07-29 09:38:19 +0200

    One more fix

--- themes/matte-black/vscode.sh
+++ themes/matte-black/vscode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 VSC_THEME="Matte Black Theme"
-VSC_EXTENSION="CleanThemes.matteblack-theme"
+VSC_EXTENSION="CleanThemes.matte-black-theme"
 source $OMAKUB_PATH/themes/set-vscode-theme.sh
