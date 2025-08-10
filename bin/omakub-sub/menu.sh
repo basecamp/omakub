79cacf2279142440c2091677410ddc31b4861572
aclaret <adria.claret@gmail.com>
2025-08-10 15:46:19 +0200

    feat: Remove import feature

--- bin/omakub-sub/menu.sh
+++ bin/omakub-sub/menu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-	SUB=$(gum choose "Theme" "Font" "Update" "Install" "Uninstall" "Import" "Manual" "Quit" --height 10 --header "" | tr '[:upper:]' '[:lower:]')
+	SUB=$(gum choose "Theme" "Font" "Update" "Install" "Uninstall" "Manual" "Quit" --height 10 --header "" | tr '[:upper:]' '[:lower:]')
 else
 	SUB=$1
 fi
