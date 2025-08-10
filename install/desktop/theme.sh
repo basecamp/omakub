c63cd510563cc33df78b5ce5ac7c2cbf7b78f684
aclaret <adria.claret@gmail.com>
2025-08-10 15:44:46 +0200

    feat: Add theme management script
    Introduces a new script, `theme.sh`, to centralize and manage the active theme in the installation process.
    
    The script performs the following actions:
    * Creates symbolic links for all available themes to a central location (`~/.config/omakub/themes`).
    * Sets the initial theme to `tokyo-night` by linking theme files and a background image to `~/.config/omakub/current`.
    * Automates the application of the current theme to the following tools:
        * Alacritty
        * Zellij
        * Neovim
        * Btop (with a check for theme availability)
    * Sources separate scripts to handle theme application for Gnome, Tophat, and VS Code.
    
    This change provides a single point of truth for theme configuration, making it easier to maintain and update the system's appearance.

---
+++ install/desktop/theme.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+INITIAL_THEME=tokyo-night
+
+# Setup theme links
+# Create themes folder into user config
+mkdir -p ~/.config/omakub/themes
+
+# Iterate all omakub themes creating/overriding symlinks for each theme into the user config themes folder
+for f in ~/.local/share/omakub/themes/*; do ln -nfs "$f" ~/.config/omakub/themes/; done
+
+# Set initial theme
+mkdir -p ~/.config/omakub/current
+ln -snf ~/.config/omakub/themes/$INITIAL_THEME ~/.config/omakub/current/theme
+ln -snf ~/.config/omakub/current/theme/background.jpg ~/.config/omakub/current/background
+
+# Set specific app links for current theme
+ln -snf ~/.config/omakub/current/theme/alacritty.toml ~/.config/alacritty/theme.toml
+
+ln -snf ~/.config/omakub/current/theme/zellij.kdl ~/.config/zellij/themes/$INITIAL_THEME.kdl
+sed -i "s/theme \".*\"/theme \"$INITIAL_THEME\"/g" ~/.config/zellij/config.kdl
+
+ln -snf ~/.config/omakub/current/theme/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
+
+if [ -f "~/.config/omakub/current/theme/btop.theme" ]; then
+  ln -snf ~/.config/omakub/current/theme/btop.theme ~/.config/btop/themes/$INITIAL_THEME.theme
+  sed -i "s/color_theme = \".*\"/color_theme = \"$INITIAL_THEME\"/g" ~/.config/btop/btop.conf
+else
+  sed -i "s/color_theme = \".*\"/color_theme = \"Default\"/g" ~/.config/btop/btop.conf
+fi
+
+source ~/.config/omakub/current/theme/gnome.sh
+source ~/.config/omakub/current/theme/tophat.sh
+source ~/.config/omakub/current/theme/vscode.sh
