dc7d8a301d6be3aa67f9548f0d684c9a640a022d
aclaret <adria.claret@gmail.com>
2025-08-15 16:09:50 +0200

    feat: update background file name

--- themes/set-gnome-theme.sh
+++ themes/set-gnome-theme.sh
@@ -7,13 +7,13 @@ gsettings set org.gnome.desktop.interface icon-theme "Yaru-$OMAKUB_THEME_COLOR"
 gsettings set org.gnome.desktop.interface accent-color "$OMAKUB_THEME_COLOR" 2>/dev/null || true
 
 BACKGROUND_DEST_DIR="$HOME/.config/omakub/current/background"
-
 BACKGROUND_FILENAME=$(basename "$OMAKUB_THEME_BACKGROUND")
+BACKGROUND_DEST_PATH="$BACKGROUND_DEST_DIR/$(echo $OMAKUB_THEME_BACKGROUND | tr '/' '-')"
 
 if [ ! -d "$BACKGROUND_DEST_DIR" ]; then mkdir -p "$BACKGROUND_DEST_DIR"; fi
 
-ln -snf $HOME/.config/omakub/current/theme/$BACKGROUND_FILENAME $BACKGROUND_DEST_DIR
+ln -snf $HOME/.config/omakub/current/theme/$BACKGROUND_FILENAME "$BACKGROUND_DEST_PATH"
 
-gsettings set org.gnome.desktop.background picture-uri $BACKGROUND_DEST_DIR/$BACKGROUND_FILENAME
-gsettings set org.gnome.desktop.background picture-uri-dark $BACKGROUND_DEST_DIR/$BACKGROUND_FILENAME
+gsettings set org.gnome.desktop.background picture-uri "$BACKGROUND_DEST_PATH"
+gsettings set org.gnome.desktop.background picture-uri-dark "$BACKGROUND_DEST_PATH"
 gsettings set org.gnome.desktop.background picture-options 'zoom'
