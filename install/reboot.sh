2942ff1fc954f3a9bd62b992f6880ce804d6875f
Luca Pattocchio <pattocchioluca@gmail.com>
2025-09-09 14:14:47 +0200

    Update warning message for X11 sessions to recommend switching to Wayland

--- install/reboot.sh
+++ install/reboot.sh
@@ -4,5 +4,12 @@
 gsettings set org.gnome.desktop.screensaver lock-enabled true
 gsettings set org.gnome.desktop.session idle-delay 300
 
+# Warning on x11 sessions to use Wayland instead
+if [ "$XDG_SESSION_TYPE" = "x11" ]; then
+  echo -e "\e[33m\nWarning: You are currently using an X11 session. It is recommended to switch to a Wayland session for the best experience with Omakub.\e[0m"
+  echo -e "\e[33mYou can select the Wayland session at the login screen by clicking on the gear icon and choosing 'Ubuntu (Wayland)'.\e[0m"
+  echo
+fi
+
 # Logout to pickup changes
 gum confirm "Ready to reboot for all settings to take effect?" && sudo reboot
--- migrations/1757320291.sh
+++ migrations/1757320291.sh
@@ -129,7 +129,7 @@ source ~/.local/share/omakub/install/preflight/migrations.sh
 # Warning on x11 sessions to use Wayland instead
 if [ "$XDG_SESSION_TYPE" = "x11" ]; then
   echo -e "\e[33m\nWarning: You are currently using an X11 session. It is recommended to switch to a Wayland session for the best experience with Omakub.\e[0m"
-  echo -e "\e[33mYou can select the Wayland session at the login screen by clicking on the gear icon and choosing 'Omakub (Wayland)'.\e[0m"
+  echo -e "\e[33mYou can select the Wayland session at the login screen by clicking on the gear icon and choosing 'Ubuntu (Wayland)'.\e[0m"
   echo
 fi
 
