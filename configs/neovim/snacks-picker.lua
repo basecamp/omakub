e52661d7eb43e723682275f94dd68e23c96ec7f9
Pauloparakleto <alparakleto@gmail.com>
2025-09-17 01:25:01 -0300

    List hidden and ignored files in file Explorer Snack Picker plugin
    Permit list hidden and ignored files by grep command, file explorer and
    telescope.

---
+++ configs/neovim/snacks-picker.lua
@@ -0,0 +1,18 @@
+return {
+	"folke/snacks.nvim",
+	opts = {
+		picker = {
+			sources = {
+				files = {
+					hidden = true,
+				},
+				explorer = {
+					hidden = true,
+				},
+				grep = {
+					hidden = true,
+				},
+			},
+		},
+	},
+}
