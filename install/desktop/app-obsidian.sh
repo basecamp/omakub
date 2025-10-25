1bab840c8e2e6f948e093977e1ba1a99d621e8f4
Luca Pattocchio <pattocchioluca@gmail.com>
2025-10-25 15:01:33 +0200

    Replace Flatpak installation with direct download and installation of Obsidian

--- install/desktop/app-obsidian.sh
+++ install/desktop/app-obsidian.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # Obsidian is a multi-platform note taking application. See https://obsidian.md
-gum spin --spinner meter --title "Obsidian installation about to start. It may take up to 20 minutes on some systems!" -- sleep 3
-flatpak install -y flathub md.obsidian.Obsidian
+cd /tmp
+OBSIDIAN_VERSION=$(curl -s https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest | grep tag_name | cut -d '"' -f 4)
+wget -O obsidian.deb "https://github.com/obsidianmd/obsidian-releases/releases/download/${OBSIDIAN_VERSION}/Obsidian_${OBSIDIAN_VERSION}_amd64.deb"
+sudo apt install -y ./obsidian.deb
+rm obsidian.deb
+cd -
