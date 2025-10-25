8eb73fef193b002551fefe75d0701a5ecb9396bc
Luca Pattocchio <pattocchioluca@gmail.com>
2025-10-25 15:48:12 +0200

    Fix typo

--- install/desktop/app-obsidian.sh
+++ install/desktop/app-obsidian.sh
@@ -3,7 +3,7 @@
 # Obsidian is a multi-platform note taking application. See https://obsidian.md
 cd /tmp
 OBSIDIAN_VERSION=$(curl -s https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest | grep tag_name | cut -d '"' -f 4)
-wget -O obsidian.deb "https://github.com/obsidianmd/obsidian-releases/releases/download/${OBSIDIAN_VERSION}/Obsidian_${OBSIDIAN_VERSION}_amd64.deb"
+wget -O obsidian.deb "https://github.com/obsidianmd/obsidian-releases/releases/download/${OBSIDIAN_VERSION}/obsidian_${OBSIDIAN_VERSION}_amd64.deb"
 sudo apt install -y ./obsidian.deb
 rm obsidian.deb
 cd -
