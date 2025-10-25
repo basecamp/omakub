188b738a0a6687466cd35f5472125614dc07c7cb
Luca Pattocchio <pattocchioluca@gmail.com>
2025-10-25 17:07:33 +0200

    Update Obsidian installation script to use correct version retrieval method
    Fix typo

--- install/desktop/app-obsidian.sh
+++ install/desktop/app-obsidian.sh
@@ -2,8 +2,8 @@
 
 # Obsidian is a multi-platform note taking application. See https://obsidian.md
 cd /tmp
-OBSIDIAN_VERSION=$(curl -s https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest | grep tag_name | cut -d '"' -f 4)
-wget -O obsidian.deb "https://github.com/obsidianmd/obsidian-releases/releases/download/${OBSIDIAN_VERSION}/obsidian_${OBSIDIAN_VERSION}_amd64.deb"
+OBSIDIAN_VERSION=$(curl -s https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest | grep -Po '"tag_name": "v\K[^"]*')
+wget -O obsidian.deb "https://github.com/obsidianmd/obsidian-releases/releases/latest/download/obsidian_${OBSIDIAN_VERSION}_amd64.deb"
 sudo apt install -y ./obsidian.deb
 rm obsidian.deb
 cd -
