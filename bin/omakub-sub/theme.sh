51221f01b732f420620113ec0a40b5e90c0c2a6f
Luca Pattocchio <pattocchioluca@gmail.com>
2025-11-08 12:46:42 +0100

    Removed deprecated comment

--- bin/omakub-sub/theme.sh
+++ bin/omakub-sub/theme.sh
@@ -21,10 +21,6 @@ if [ -n "$THEME" ] && [ "$THEME" != "<<-back" ]; then
   source $OMAKUB_PATH/themes/$THEME/gnome.sh
   source $OMAKUB_PATH/themes/$THEME/tophat.sh
   source $OMAKUB_PATH/themes/$THEME/vscode.sh
-
-  # Forgo setting the Chrome theme until we might find a less disruptive way of doing it.
-  # Having to quit Chrome, and all Chrome-based apps, is too much of an inposition.
-  # source $OMAKUB_PATH/themes/$THEME/chrome.sh
 fi
 
 source $OMAKUB_PATH/bin/omakub-sub/menu.sh
