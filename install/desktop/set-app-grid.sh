a392df9be95762dc01125e1999c3ffaa57cab1b6
Luca Pattocchio <pattocchioluca@gmail.com>
2025-11-09 20:37:21 +0100

    Modify Xtra app folder settings
    Updated the Xtra app folder to include additional applications on Ubuntu 25.10

--- install/desktop/set-app-grid.sh
+++ install/desktop/set-app-grid.sh
@@ -8,6 +8,7 @@ sudo rm -rf /usr/share/applications/org.flameshot.Flameshot.desktop
 
 # Remove the ImageMagick icon
 sudo rm -rf /usr/share/applications/display-im6.q16.desktop
+sudo rm -rf /usr/share/applications/display-im7.q16.desktop
 
 # Replacing this with btop
 sudo rm -rf /usr/share/applications/org.gnome.SystemMonitor.desktop
@@ -21,7 +22,7 @@ gsettings set org.gnome.desktop.app-folders folder-children "['Utilities', 'Sund
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Updates/ name 'Install & Update'
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Updates/ apps "['org.gnome.Software.desktop', 'software-properties-drivers.desktop', 'software-properties-gtk.desktop', 'update-manager.desktop', 'firmware-updater_firmware-updater.desktop', 'snap-store_snap-store.desktop']"
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Xtra/ name 'Xtra'
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Xtra/ apps "['gnome-language-selector.desktop', 'org.gnome.PowerStats.desktop', 'yelp.desktop', 'org.gnome.eog.desktop']"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Xtra/ apps "['org.Characters.desktop', 'gnome-language-selector.desktop', 'org.gnome.PowerStats.desktop', 'org.gnome.Logs.desktop', 'yelp.desktop', 'org.gnome.Yelp.desktop', 'org.gnome.eog.desktop', 'org.gnome.Sysprof.desktop' ]"
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/LibreOffice/ name 'LibreOffice'
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/LibreOffice/ apps "['libreoffice-base.desktop', 'libreoffice-calc.desktop', 'libreoffice-draw.desktop', 'libreoffice-impress.desktop', 'libreoffice-math.desktop', 'libreoffice-startcenter.desktop', 'libreoffice-writer.desktop', 'libreoffice-xsltfilter.desktop']"
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/WebApps/ name 'Web Apps'
