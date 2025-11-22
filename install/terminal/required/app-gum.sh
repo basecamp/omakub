003cac37756ed45fbcd40abc407dd03964eab1b8
Pan Som <andrey.golovany@gmail.com>
2025-11-22 03:07:53 +0300

    Switching to the official repository

--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Gum is used for the Omakub commands for tailoring Omakub after the initial install
-cd /tmp
-GUM_VERSION="0.17.0"
-wget -qO gum.deb "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_amd64.deb"
-sudo apt-get install -y --allow-downgrades ./gum.deb
-rm gum.deb
-cd -
+
+if [ ! -f /etc/apt/sources.list.d/charm.list]; then
+    [ -f /etc/apt/keyrings/charm.gpg ] && sudo rm /etc/apt/keyrings/charm.gpg
+    curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+fi
+sudo apt-get update
+sudo apt-get install -y gum
