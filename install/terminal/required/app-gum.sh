2da11be200d4eb5e8b8e3767c38b3557c14d8f94
Pan Som <andrey.golovany@gmail.com>
2025-11-22 06:44:50 +0300

    fix typo

--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -2,7 +2,7 @@
 
 # Gum is used for the Omakub commands for tailoring Omakub after the initial install
 
-if [ ! -f /etc/apt/sources.list.d/charm.list]; then
+if [ ! -f /etc/apt/sources.list.d/charm.list ]; then
     [ -f /etc/apt/keyrings/charm.gpg ] && sudo rm /etc/apt/keyrings/charm.gpg
     curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
     echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
