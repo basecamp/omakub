9f7b76ad10928b149d1bddc4ca5af4f791387295
Pan Som <andrey.golovany@gmail.com>
2025-11-22 06:59:55 +0300

    feat(apt): migrate repos to modern .sources format

--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -4,8 +4,15 @@
 
 if [ ! -f /etc/apt/sources.list.d/charm.list ]; then
     [ -f /etc/apt/keyrings/charm.gpg ] && sudo rm /etc/apt/keyrings/charm.gpg
-    curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
-    echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+	curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg > /dev/null
+	printf '%s\n' \
+		"Types: deb" \
+		"URIs: https://repo.charm.sh/apt" \
+    	"Suites: *" \
+    	"Components: *" \
+   		"Architectures: amd64" \
+		"Signed-By: /etc/apt/keyrings/charm.gpg" \
+		| sudo tee /etc/apt/sources.list.d/charm.sources > /dev/null
 fi
 sudo apt-get update
 sudo apt-get install -y gum
