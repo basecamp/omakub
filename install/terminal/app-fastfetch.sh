836ff9b07a7e0b25500c95fe303945010dc760f9
Pan Som <andrey.golovany@gmail.com>
2025-11-25 20:39:51 +0300

    fix: improve signature key retrieval method

--- install/terminal/app-fastfetch.sh
+++ install/terminal/app-fastfetch.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
 # Display system information in the terminal
-#
-# Load OS release information
-. /etc/os-release
 if [ ! -f /etc/apt/sources.list.d/zhangsongcui3371-fastfetch.sources ]; then
-    [ -f /etc/apt/keyrings/zhangsongcui3371-fastfetch.asc ] && sudo rm /etc/apt/keyrings/zhangsongcui3371-fastfetch.asc
-    curl -fsSL \
-        "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xeb65ee19d802f3eb1a13cfe47e2e5cb4d4865f21" \
-        | sudo tee /etc/apt/keyrings/zhangsongcui3371-fastfetch.asc > /dev/null
-
-    printf '%s\n' \
-        "Types: deb deb-src" \
-        "URIs: https://ppa.launchpadcontent.net/zhangsongcui3371/fastfetch/ubuntu" \
-        "Suites: ${VERSION_CODENAME}" \
-        "Components: main" \
-        "Architectures: amd64" \
-        "Signed-By: /etc/apt/keyrings/zhangsongcui3371-fastfetch.asc" \
-        | sudo tee /etc/apt/sources.list.d/zhangsongcui3371-fastfetch.sources > /dev/null
+	sudo apt install -y gnupg
+	[ -f /etc/apt/keyrings/zhangsongcui3371-fastfetch.gpg ] && sudo rm /etc/apt/keyrings/zhangsongcui3371-fastfetch.gpg
+	gpg --keyserver keyserver.ubuntu.com --recv 0x7e2e5cb4d4865f21
+	gpg --export 0x7e2e5cb4d4865f21 | sudo tee /usr/share/keyrings/zhangsongcui3371-fastfetch.gpg >/dev/null
+	printf '%s\n' \
+		"Types: deb deb-src" \
+		"URIs: https://ppa.launchpadcontent.net/zhangsongcui3371/fastfetch/ubuntu" \
+		"Suites: ${VERSION_CODENAME}" \
+		"Components: main" \
+		"Architectures: amd64" \
+		"Signed-By: /etc/apt/keyrings/zhangsongcui3371-fastfetch.gpg" |
+		sudo tee /etc/apt/sources.list.d/zhangsongcui3371-fastfetch.sources >/dev/null
 fi
 
 sudo apt update -y
@@ -25,7 +21,7 @@ sudo apt install -y fastfetch
 
 # Only attempt to set configuration if fastfetch is not already set
 if [ ! -f "$HOME/.config/fastfetch/config.jsonc" ]; then
-  # Use Omakub fastfetch config
-  mkdir -p ~/.config/fastfetch
-  cp ~/.local/share/omakub/configs/fastfetch.jsonc ~/.config/fastfetch/config.jsonc
+	# Use Omakub fastfetch config
+	mkdir -p ~/.config/fastfetch
+	cp ~/.local/share/omakub/configs/fastfetch.jsonc ~/.config/fastfetch/config.jsonc
 fi
